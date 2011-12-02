f275c9fff5827d2f056ecdcd3edd750273030007
Jean-Francois Ducarroz <mrhg74@motorola.com>
2011-12-01 16:09:12 -0800

    Done more implementation work of the RichTextField: -does not extend anymore editableText: -added support for basic actions -added some interaction with delegate

--- lab/lib/ui/richtextfield.reel/richtextfield.js
+++ lab/lib/ui/richtextfield.reel/richtextfield.js
@@ -9,60 +9,347 @@
     @requires montage/ui/editable-text
 */
 var Montage = require("montage").Montage,
-    EditableText = require("ui/editable-text").EditableText;
+    Component = require("ui/component").Component;
 /**
     @class module:"montage/ui/richtextfield.reel".RichTextfield
-    @extends module:montage/ui/editable-text.EditableText
+    @extends module:montage/ui/component.Component
 */
-var RichTextfield = exports.RichTextfield = Montage.create(EditableText,/** @lends module:"montage/ui/richtextfield.reel".RichTextfield# */ {
-    /**
-    Description TODO
-    @type {Property}
-    @default null
-    */
-    delegate: {
-        enumerable: true,
-        value: null
-    },
+exports.RichTextfield = Montage.create(Component,/** @lends module:"montage/ui/richtextfield.reel".RichTextfield# */ {
 
     /**
-    Description TODO
-    @private
+      Description TODO
+      @private
     */
-    _drawSpecific: {
+    _hasSelectionChangeEvent: {
         enumerable: false,
-        value: function() {
-            this.element.classList.add('montage-richtextfield');
+        value: null     // Need to be preset to null, will be set to true or false later on
+    },
+
+    /**
+      Description TODO
+      @private
+    */
+    _needSelectionReset: {
+        enumerable: false,
+        value: false
+    },
+    
+    /**
+      Description TODO
+      @private
+    */
+    _hasFocus: {
+        enumerable: false,
+        value: false
+    },
+
+    /**
+      Description TODO
+     @type {Function}
+    */
+    hasFocus: {
+        enumerable: true,
+        get: function() {
+            return this._hasFocus;
         }
     },
 
     /**
-    Description TODO
-    Overwrite
-    @private
+      Description TODO
+      @private
     */
-    _setElementValue: {
-        value: function(v) {
-            this.element.innerHTML = v;
+    _hasChanged: {
+        enumerable: false,
+        value: false
+    },
+
+    /**
+      Description TODO
+     @type {Function}
+    */
+    hasChanged: {
+        enumerable: true,
+        get: function() {
+            return this._hasChanged;
         }
     },
+
+    /**
+      Description TODO
+      @private
+    */
+    _value: {
+        enumerable: false,
+        value: ""
+    },
+
+    /**
+      Description TODO
+     @type {Function}
+    */
+    value: {
+        enumerable: true,
+        get: function() {
+            if (this._hasChanged) {
+                this._value = this.element.innerHTML;
+                this._hasChanged = false;
+            }
+            return this._value;
+        },
+        set: function(value) {
+            if (this._value !== value) {
+                this._value = value;
+                this._needSelectionReset = true;
+                this.needsDraw = true;
+            }
+        }
+    },
+
+    /**
+      Description TODO
+      @private
+    */
+    _commands: {
+        enumerable: false,
+        value: {"bold": true, "italic": true, "underline": true, "strikethrough": true, "indent": true, "outdent": true,
+            "insertorderedlist": true, "insertunorderedlist": true}
+    },
+
+    /**
+      Description TODO
+     @type {Function}
+    */
+    actions: {
+        enumerable: false,
+        get: function() {
+            return this._commands;
+        }
+    },
+
+    /**
+      Description TODO
+     @type {Function}
+    */
+    enabledActions: {
+        enumerable: true,
+        set: function(enabledActions) {
+            var actions = this._commands,
+                nbrEnabledActions = enabledActions.length,
+                action,
+                i;
+
+            for (action in this._commands) {
+                this._commands[action] = false;
+            }
+
+            for (i = 0; i < nbrEnabledActions; i ++) {
+                action = enabledActions[i];
+                if (actions[action] !== undefined) {
+                    actions[action] = true;
+                }
+            }
+
+            console.log("COMMANDS:", this._commands);
+        }
+    },
+
 
     // Callbacks
     /**
     Description TODO
-    Overwrite
     @function
     */
     prepareForDraw: {
         enumerable: false,
         value: function() {
             var el = this.element;
-
-            // call super first
-            EditableText.prepareForDraw.call(this);
-            
+            el.classList.add('montage-richtextfield');
             el.setAttribute("contentEditable", "true");
+
+            el.addEventListener("focus", this);
+        }
+    },
+
+    /**
+    Description TODO
+    @function
+    */
+    draw: {
+        enumerable: false,
+        value: function() {
+            this.element.innerHTML = this._value;
+        }
+    },
+
+    // Event handlers
+    /**
+    Description TODO
+    @function
+    */
+    handleEvent: {
+        value: function(event) {
+
+            var handlerName = "handle" + event.type.toCapitalized();
+
+            if (typeof this[handlerName] === "function") {
+                this[handlerName](event);
+            } else {
+                if (this._hasSelectionChangeEvent === false) {
+                    var type = event.type;
+                    if (type == "keyup" || type == "mouseup" || type == "touchend") {
+                        this.handleSelectionchange();
+                    }
+                }
+            }
+        }
+    },
+    /**
+    Description TODO
+    @function
+    */
+    handleFocus: {
+        enumerable: false,
+        value: function() {
+            var el = this.element;
+
+            this._hasFocus = true;
+            if (this._needSelectionReset) {
+                // JFD TODO: reset the user selection, set the caret at the end of the last text element
+                this._needSelectionReset = false;
+            }
+
+            el.addEventListener("blur", this);
+            el.addEventListener("input", this);
+            el.addEventListener("keypress", this);
+
+            document.addEventListener("selectionchange", this, false);
+            // Check if the browser does not supports the DOM event selectionchange
+            if (this._hasSelectionChangeEvent === null) {
+                var thisRef = this;
+                setTimeout(function(){
+                    if (thisRef._hasSelectionChangeEvent === null) {
+                        thisRef._hasSelectionChangeEvent = false;
+                    }
+                }, 0);
+            }
+            if (this._hasSelectionChangeEvent === false) {
+                // We need to listen to more event in order to simulate a selectionchange event
+                el.addEventListener(window.Touch ? "touchend" : "mouseup", this);
+                el.addEventListener("keydup", this);
+            }
+
+            // Turn off image resize (if supported)
+            document.execCommand("enableObjectResizing", false, false);
+            // Force use css for styling (if supported)
+            document.execCommand("styleWithCSS", false, true);
+        }
+    },
+
+    /**
+    Description TODO
+    @function
+    */
+    handleBlur: {
+        enumerable: false,
+        value: function() {
+            var el = this.element;
+
+            this._hasFocus = false;
+
+            el.removeEventListener("blur", this);
+            el.removeEventListener("input", this);
+            el.removeEventListener("keypress", this);
+            
+            document.removeEventListener("selectionchange", this);
+
+            if (this._hasSelectionChangeEvent === false) {
+                el.addEventListener(window.Touch ? "touchend" : "mouseup", this);
+                el.addEventListener("keydup", this);
+            }
+        }
+    },
+
+    /**
+    Description TODO
+    @function
+    */
+    handleKeypress: {
+        enumerable: false,
+        value: function() {
+            this._hasChanged = true;
+            if (this._hasSelectionChangeEvent === false) {
+                this.handleSelectionchange();
+            }
+            // JFD TODO: We might want to use a timer to avoid doing too much processing when the user type...
+            this.callDelegateMethod("didValueChanged", this);
+        }
+    },
+
+    /**
+    Description TODO
+    @function
+    */
+    handleInput: {
+        enumerable: false,
+        value: function() {
+            this._hasChanged = true;
+            if (this._hasSelectionChangeEvent === false) {
+                this.handleSelectionchange();
+            }
+            this.callDelegateMethod("didValueChanged", this);
+        }
+    },
+
+    /**
+    Description TODO
+    @function
+    */
+    handleSelectionchange: {
+        enumerable: false,
+        value: function() {
+            if (this._hasSelectionChangeEvent == null) {
+                this._hasSelectionChangeEvent = true;
+            }
+
+            this.callDelegateMethod("didSelectionChanged", this);
+        }
+    },
+
+    // Actions
+    /**
+    Description TODO
+    @function
+    */
+    handleAction: {
+        enumerable: false,
+        value: function(event) {
+            var action = event.currentTarget.identifier;
+            if (action) {
+                this.doAction(action);
+            }
+        }
+    },
+
+    /**
+    Description TODO
+    @function
+    */
+    doAction: {
+        enumerable: true,
+        value: function(action) {
+
+            // Check if the action is valid and enabled
+            if (this._commands[action] === true) {
+                document.execCommand("styleWithCSS", false, true);
+                document.execCommand(action, false, false);
+                document.execCommand("styleWithCSS", false, false);
+
+                if (this._hasSelectionChangeEvent === false) {
+                    this.handleSelectionchange();
+                }
+            }
         }
     }
+
 });
 
