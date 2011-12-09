e7dbd6921bd2a42589dda48ac021c27bbc0ee7b5
Mike Czepiel <mczepiel@motorola.com>
2011-12-08 17:30:54 -0800

    PhotoFX gained a proxy icon for the current image
    I'm hiding an actual image underneath the live thumbnail canvas that you
    can drag to your desktop or other applications. So you kind of get a
    pretty decent save for the application, albeit it one that's most familiar to Mac users.
    
    Performance leaves a little something to be desired as far as the calling of
    toDataURL when it looks like you're about to drag the image.
    
    Also, this doesn't really work on iOS right now, in any capacity.

--- examples/photofx/photo-info-panel.reel/photo-info-panel.js
+++ examples/photofx/photo-info-panel.reel/photo-info-panel.js
@@ -8,9 +8,103 @@ var Component = require("montage/ui/component").Component;
 
 exports.PhotoInfoPanel = Montage.create(Component, {
 
-    photo: {
+    _photo: {
         enumerable: false,
         value: null
+    },
+
+    photo: {
+        get: function() {
+            return this._photo;
+        },
+        set: function(value) {
+            if (value === this._photo) {
+                return;
+            }
+
+            this._photo = value;
+            this._photoChanged = true;
+            this.needsDraw = true;
+        }
+    },
+
+    _dragProxy: {
+        enumerable: false,
+        value: null
+    },
+
+    _canvas: {
+        enumerable: false,
+        value: null
+    },
+
+    _modifiedCanvas: {
+        enumerable: false,
+        value: null
+    },
+
+    handleImagemodified: {
+        value: function(evt) {
+            this._modifiedCanvas = evt.detail.editor._canvas;
+            this.needsDraw = true;
+        }
+    },
+
+    prepareForDraw: {
+        value: function() {
+            this.element.addEventListener("mouseover", this, false);
+            document.application.addEventListener("imagemodified", this, false);
+        }
+    },
+
+    handleMouseover: {
+        value: function() {
+            if (this.photo) {
+                this._prepareDownload();
+            }
+        }
+    },
+
+    willDraw: {
+        value: function() {
+            if (this._modifiedCanvas) {
+                this._canvasWidth = this._modifiedCanvas.offsetWidth;
+                this._canvasHeight = this._modifiedCanvas.offsetHeight;
+            }
+        }
+    },
+
+    _prepareDownload: {
+        value: function() {
+            // TODO move this to the draw loop too
+            this._dragProxy.setAttribute("src", this._canvas.toDataURL());
+        }
+    },
+
+    draw: {
+        value: function() {
+            if (this._modifiedCanvas) {
+                var originalContext = this._modifiedCanvas.getContext("2d"),
+                    originalData = originalContext.getImageData(0, 0, this._canvasWidth, this._canvasHeight);
+
+                this._canvas.width = this._canvasWidth;
+                this._canvas.height = this._canvasHeight;
+
+                var thumbnailContext = this._canvas.getContext("2d");
+                    thumbnailContext.putImageData(originalData, 0, 0);
+                this._modifiedCanvas = null;
+            }
+
+            if (this._photoChanged) {
+                if (this.photo) {
+                    this._dragProxy.setAttribute("alt", this.photo.title);
+                } else {
+                    this._dragProxy.removeAttribute("src");
+                }
+                this._photoChanged = false;
+            }
+
+        }
     }
 
 });
