cddb8eaea1738972bfdfb6340865f7d9db4b6219
Kris Kowal <kris.kowal@motorola.com>
2011-12-13 12:15:42 -0800

    Added some basic promise tests.

--- test/run.js
+++ test/run.js
@@ -36,6 +36,7 @@ if (spec) {
 
         require.async("core/core-require-spec"),
         require.async("core/core-spec"),
+        require.async("core/promise-spec"),
 
         require.async("data/blueprint-spec"),
         require.async("data/context-spec"),
