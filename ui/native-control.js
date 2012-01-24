b25de31b7b27c0df6d0ed05df768fe689c653e7d
Stuart Knightley <stuartk@motorola.com>
2012-01-23 16:38:25 -0800

    Update native-control addProperties
    Now pulls in attribute values from the element on component initialization
    even if the property is already defined. It doesn't overwrite the
    existing properties with its own getters and setters.

--- ui/native-control.js
+++ ui/native-control.js
@@ -88,7 +88,6 @@ exports.NativeControl = Montage.create(Component, {
                         // if requested dataType is boolean (eg: checked, readonly etc)
                         // coerce the value to boolean
                         if(desc && "boolean" === desc.dataType) {
-                            //val = (val || val === "");
                             val = ( (val || val === "") ? true : false);
                         }
 
@@ -130,18 +129,20 @@ exports.NativeControl = Montage.create(Component, {
 
             for(prop in stdAttrs) {
                 if(stdAttrs.hasOwnProperty(prop)) {
-                    if(isUndefined(this[prop])) {
-                        obj = stdAttrs[prop];
-                        // Make sure that the descriptor is of the correct form.
-                        if(obj === null || isString(obj)) {
-                            desc = {value: obj, dataType: "string"};
-                            stdAttrs[prop] = desc;
-                        } else {
-                            desc = obj;
-                        }
-                        this.addProperty(prop, desc);
+                    obj = stdAttrs[prop];
+                    // Make sure that the descriptor is of the correct form.
+                    if(obj === null || isString(obj)) {
+                        desc = {value: obj, dataType: "string"};
+                        stdAttrs[prop] = desc;
+                    } else {
+                        desc = obj;
                     }
 
+                    // Only add the internal prop, and getter and setter if
+                    // they don't already exist.
+                    if(isUndefined(this[prop])) {
+                        this.addProperty(prop, desc);
+                    }
                 }
             }
         }
@@ -167,7 +168,7 @@ exports.NativeControl = Montage.create(Component, {
                     }
                 }
             }
-            
+
             // check if this element has textContent
             var textContent = this.element.textContent;
             // set textContent only if it is defined as part of element properties
@@ -223,7 +224,7 @@ exports.NativeControl = Montage.create(Component, {
                                 //https://developer.mozilla.org/en/DOM/element.setAttribute
                                 el.setAttribute(i, val);
                             }
-                            
+
                         }
                     }
 
