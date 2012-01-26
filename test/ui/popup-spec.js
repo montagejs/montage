d0c2a99d80c95fcbe841e22d2eb767805e526845
François Frisch <francois@motorola.com>
2012-01-26 15:37:49 -0800

    Fixing jasmine specs for popup resize.

--- test/ui/popup-spec.js
+++ test/ui/popup-spec.js
@@ -9,32 +9,8 @@ var Montage = require("montage").Montage,
     Popup = Popup = require("montage/ui/popup/popup.reel").Popup,
     ActionEventListener = require("montage/core/event/action-event-listener").ActionEventListener;
 
-/**
-* A Delegate to position the popup using custom logic
-*/
-var TestPopupPositionDelegate = Montage.create(Montage, {
-    positionPopup: {
-       value: function(popup, anchor, anchorPosition) {
-           if(anchor && anchorPosition) {
-               if(window.innerHeight > 500 ){
-                   return {
-                       top: 10,
-                       left: anchorPosition[0]
-                   };                   
-               } else {
-                   return {
-                       bottom: 10,
-                       left: anchorPosition[0]
-                   };
-               }
-           }
-           return {top: 0, left: 0};
-       }
-    }
-});
-var TestPopupPositionDelegate = Montage.create(TestPopupPositionDelegate);
 
-var testPage = TestPageLoader.queueTest("popuptest", function() {
+var testPage = TestPageLoader.queueTest("popuptest", {newWindow: true}, function() {
     var test = testPage.test;
     describe("ui/popup-spec", function() {
         it("should load", function() {
@@ -42,29 +18,23 @@ var testPage = TestPageLoader.queueTest("popuptest", function() {
         });
 
         describe("once loaded", function() {
-            describe("position delegate", function() {
-                it("should call the position popup to continually determine position", function() {
-                    testPage.resizeTo(800, 600);
-                    var popup = test.testPopup.popup;
-                    if(!popup) {
-                        popup = Popup.create();
-                        popup.content = testPage.testPopup;
-                        popup.anchor = testPage.popupButton.element;
-                        popup.delegate = TestPopupPositionDelegate;
-                    }
-                    popup.show();
-                    
+            describe("Popup", function() {
+                it("should position based on delegate logic", function() {
+                    testPage.testWindow.resizeTo(800, 600);
+                    test.showPopup();
+
                     testPage.waitForDraw(2);
-                    runs(function() {                        
+                    runs(function() {
                         var popupPosition = EventInfo.positionOfElement(test.testPopup.popup.element);
                         expect(popupPosition.y).toBe(10);
-                        
-                        testPage.resize(800,400);
-                        testPage.waitForDraw(2);
-                        runs(function() {
-                            popupPosition = EventInfo.positionOfElement(test.testPopup.popup.element);
-                            expect(popupPosition.y).toBe(320);
-                        });
+                    });
+                });
+                it("should continually determine position at every resize", function() {
+                    testPage.testWindow.resizeTo(800,400);
+                    testPage.waitForDraw(1);
+                    runs(function() {
+                        popupPosition = EventInfo.positionOfElement(test.testPopup.popup.element);
+                        expect(popupPosition.y).toBe(122);
                     });
                 });
             });
