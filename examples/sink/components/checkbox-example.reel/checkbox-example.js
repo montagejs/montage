d7360e23134339876298e8c579aa0b8d0cca83f9
Kishore Subramanian <kishore.subramanian@motorola.com>
2012-02-02 11:51:00 -0800

    Improvements/Fixes to Kitchen Sink
    - Changes to make all demos consistent w.r.t look-n-feel
    - Added section for Substitution component
    
    (cherry picked from commit 570f2d8d823fb0ae6df60f72eeee8d8fab61c44c)

--- examples/sink/components/checkbox-example.reel/checkbox-example.js
+++ examples/sink/components/checkbox-example.reel/checkbox-example.js
@@ -7,6 +7,22 @@ var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
 exports.CheckboxExample = Montage.create(Component, {
+    
+    jsonSelectedItems: {
+        value: null
+    },
+
+    _selectedItems: {value: null},
+    selectedItems: {
+        get: function(){ return this._selectedItems; },
+        set: function(v) {
+
+            this._selectedItems = v;
+            this.jsonSelectedItems = JSON.stringify(this._selectedItems||[]);
+            console.log('selected items = ', v);
+        }
+    },
+    
     prepareForDraw: {
         value: function() {
             // Invoke Google pretty printer on source code samples
