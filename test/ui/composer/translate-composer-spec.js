0cfd5df40fa8c9b697725dbe54063719b3a010de
Stuart Knightley <stuartk@motorola.com>
2012-02-17 10:36:51 -0800

    Add some tests for translate-composer

--- test/ui/composer/translate-composer-spec.js
+++ test/ui/composer/translate-composer-spec.js
@@ -54,13 +54,94 @@ var testPage = TestPageLoader.queueTest("translate-composer-test", function() {
             describe("translateX", function() {
                 it("updates as the mouse moves", function() {
                     testPage.mouseEvent({target: test.example.element}, "mousedown");
-
+                    dragElementOffsetTo(test.example.element, 20, 0, null, null, function() {
+                        expect(test.translate_composer.translateX).toBeGreaterThan(19);
+                    });
+                });
+                it("can be set", function() {
+                    test.translate_composer.translateX = 5;
+                    expect(test.x).toBe("5px");
                 });
             });
-            describe("translateX");
-            describe("maxTranslateX");
-            describe("maxTranslateY");
-            describe("axis");
+            describe("translateY", function() {
+                it("updates as the mouse moves", function() {
+                    testPage.mouseEvent({target: test.example.element}, "mousedown");
+                    dragElementOffsetTo(test.example.element, 0, 20, null, null, function() {
+                        expect(test.translate_composer.translateY).toBeGreaterThan(19);
+                    });
+                });
+                it("can be set", function() {
+                    test.translate_composer.translateY = 5;
+                    expect(test.y).toBe("5px");
+                });
+            });
+            describe("maxTranslateX", function() {
+                it ("limits translateX", function() {
+                    testPage.mouseEvent({target: test.example.element}, "mousedown");
+                    dragElementOffsetTo(test.example.element, 500, 0, null, null, function() {
+                        // wait for the bounce to finish
+                        waits(test.translate_composer.bouncingDuration);
+                        runs(function() {
+                            expect(test.translate_composer.translateX).not.toBeGreaterThan(350);
+                        });
+                    });
+                });
+            });
+            describe("maxTranslateY", function() {
+                it ("limits translateY", function() {
+                    testPage.mouseEvent({target: test.example.element}, "mousedown");
+                    dragElementOffsetTo(test.example.element, 0, 500, null, null, function() {
+                        // wait for the bounce to finish
+                        waits(test.translate_composer.bouncingDuration);
+                        runs(function() {
+                            expect(test.translate_composer.translateY).not.toBeGreaterThan(350);
+                        });
+                    });
+                });
+            });
+
+            describe("axis", function() {
+
+                it("limits movement to horizonal when set to 'horizontal'", function() {
+                    test.translate_composer.translateX = 0;
+                    test.translate_composer.translateX = 0;
+                    test.translate_composer.axis = "horizontal";
+
+                    testPage.mouseEvent({target: test.example.element}, "mousedown");
+                    dragElementOffsetTo(test.example.element, 50, 50, null, null, function() {
+                        runs(function() {
+                            expect(test.translate_composer.translateX).toBeGreaterThan(49);
+                            expect(test.translate_composer.translateY).toBe(0);
+                        });
+                    });
+                });
+                it("limits movement to vertical when set to 'vertical'", function() {
+                    test.translate_composer.translateX = 0;
+                    test.translate_composer.translateX = 0;
+                    test.translate_composer.axis = "vertical";
+
+                    testPage.mouseEvent({target: test.example.element}, "mousedown");
+                    dragElementOffsetTo(test.example.element, 50, 50, null, null, function() {
+                        runs(function() {
+                            expect(test.translate_composer.translateX).toBe(0);
+                            expect(test.translate_composer.translateY).toBeGreaterThan(49);
+                        });
+                    });
+                });
+                it("does not limit movement when set to an unknown value", function() {
+                    test.translate_composer.translateX = 0;
+                    test.translate_composer.translateX = 0;
+                    test.translate_composer.axis = null;
+
+                    testPage.mouseEvent({target: test.example.element}, "mousedown");
+                    dragElementOffsetTo(test.example.element, 50, 50, null, null, function() {
+                        runs(function() {
+                            expect(test.translate_composer.translateX).toBeGreaterThan(49);
+                            expect(test.translate_composer.translateY).toBeGreaterThan(49);
+                        });
+                    });
+                });
+            });
 
         });
     });
