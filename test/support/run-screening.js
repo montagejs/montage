126b903fa013e310aad65752178dc996ab0b6c7c
Stuart Knightley <stuartk@motorola.com>
2012-02-29 13:44:18 -0800

    Transition automated testing to webdriver
    so that we can run different browsers.

--- test/support/run-screening.js
+++ test/support/run-screening.js
@@ -3,14 +3,28 @@
 var util = require("util"),
     fs = require("fs"),
     http = require("http"),
-    Q = require("./q.js");
+    Q = require("./q.js"),
+    wd_original = require("./wd.js");
 
-var SCREENING_HOST = "127.0.0.1",
-    SCREENING_PORT = "8081",
-    API_URL = "/screening/api/v1",
-    API_KEY = 5150;
+// Make webdriver use promises
+var webdriver = wd_original.webdriver;
+for (var propName in wd_original.webdriver.prototype) {
+    var prop = wd_original.webdriver.prototype[propName];
+    if (wd_original.webdriver.prototype.hasOwnProperty(propName) && typeof prop === "function") {
+        webdriver.prototype[propName] = Q.node(prop);
+    }
+}
+var wd = {
+    remote: function(host, port, username, accessKey) {
+        return new webdriver(host, port, username, accessKey);
+    }
+};
 
-var TEST_URL = "127.0.0.1:80/montage/test/run.html";
+var WEBDRIVER_HUB_HOST = "127.0.0.1",
+    WEBDRIVER_HUB_PORT = "4444",
+    BROWSER = "chrome";
+
+var TEST_URL = "127.0.0.1:80/montage/test/run-xml.html";
 
 var DEBUG = false;
 
@@ -27,129 +41,63 @@ if (opts.length > 0) {
         DEBUG = true;
         opts.shift();
     }
-    SCREENING_HOST = opts.shift() || SCREENING_HOST;
-    SCREENING_PORT = opts.shift() || SCREENING_PORT;
+    WEBDRIVER_HUB_HOST = opts.shift() || WEBDRIVER_HUB_HOST;
+    WEBDRIVER_HUB_PORT = opts.shift() || WEBDRIVER_HUB_PORT;
     TEST_URL = opts.shift() || TEST_URL;
 }
 
-// setup
-var screening_request = function(path, method, body) {
-    var options = {
-        hostname: SCREENING_HOST,
-        port: SCREENING_PORT,
-        path: [API_URL, path].join("/") + "?api_key=" + API_KEY,
-        method: method || "GET"
-    };
+var browser = wd.remote(WEBDRIVER_HUB_HOST, WEBDRIVER_HUB_PORT);
 
-    if (DEBUG) {
-        console.log("Request URL", options.hostname, options.port, options.path);
-    }
+// get the browser
+browser.init({browserName: BROWSER}).then(function(sessionId) {
+    return browser.get(TEST_URL);
+}).then(function() {
+    // run the script
+    console.log("Running " + TEST_URL + " on " + WEBDRIVER_HUB_HOST + ":" + WEBDRIVER_HUB_PORT + " on " + BROWSER);
 
-    var deferred = Q.defer();
-    var req = http.request(options, function(res) {
-        res.setEncoding("utf8");
-        var buffer = "";
-        res.on("data", function(chunk) {
-            buffer += chunk;
-        });
-        res.on("end", function() {
-            if (DEBUG) {
-                console.log("Response data:", buffer);
-            }
-
-            var data;
-            try {
-                data = JSON.parse(buffer);
-            } catch (e) {
-                // not JSON
-                deferred.resolve(buffer);
-                return;
-            }
-
-            if (data.error) {
-                deferred.reject(data.error);
-                return;
-            }
-            deferred.resolve(data);
-        });
-    });
-    req.on("error", deferred.reject);
-
-    req.setHeader("Content-Type", "application/json");
-    req.end(body, "utf8");
-    return deferred.promise;
-};
-
-var escapeBadXmlChars = function(string) {
-    return string.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-};
-
-// store script here or in a file
-var script = fs.readFileSync("jasmine-tests.screening.js", "utf8").replace("%TEST_URL%", TEST_URL);
-
-// find the agent
-screening_request("agents").then(function(data) {
-    return data[0];
-}).then(function(agent) {
-    // run the script on screening
-    console.log("Running " + TEST_URL + " on " + SCREENING_HOST + ":" + SCREENING_PORT + " on " + agent.id);
-    return screening_request(
-        ["agents", encodeURI(agent.id), "execute_serialized_code"].join("/"),
-        "POST",
-        JSON.stringify({code: script, name: "jasmine-tests.screening.js"})
-    );
-}).then(function(data) {
     // poll until it's done
     var done = Q.defer();
-    var poll = setInterval(function() {
-        screening_request("test_results/" + data.testId).then(function(data) {
-            process.stdout.write(".");
-            if (data.status !== "RUNNING") {
+    var previousUpdate = -1;
+
+    var poll = function() {
+        process.stdout.write(".");
+        browser.execute("return jasmine.getEnv().lastUpdate").then(function(lastUpdate) {
+            if (DEBUG) {
+                console.log(lastUpdate);
+            }
+
+            if (lastUpdate === previousUpdate) {
+                // newline
                 console.log();
                 clearInterval(poll);
-                done.resolve(data);
+                done.resolve();
+            } else {
+                previousUpdate = lastUpdate;
+                setTimeout(poll, 5000);
             }
         });
-    }, 1000);
+    };
+    poll();
 
     return done.promise;
-}).then(function(data) {
-    // get or create xml and save to file
+}).then(function() {
+    return browser.execute("return __jasmine_reports;");
+}).then(function(reports) {
+    browser.quit();
 
-    var output = "";
-
-    // building xml by concatenating strings, woo!
-    // but seriously, this is a really bad idea, but I don't want to include a
-    // full library just for this.
-    output += '<testsuite>\n';
-
-    var asserts = data.asserts;
-    for (var i = 0, len = asserts.length; i < len; i++) {
-        var assert = asserts[i];
-        var short_message = "unknown";
-        if (assert.message !== null) {
-            short_message = assert.message.split("\n", 1)[0];
-        }
-
-        if (assert.success) {
-            output += '  <testcase classname="run.html" name="' + escapeBadXmlChars(short_message) + '" />\n';
-        } else {
-
-            // TODO escape string
-            output += '  <testcase classname="run.html" name="' + escapeBadXmlChars(short_message) + '">\n';
-            output += '    <failure type="' + escapeBadXmlChars(assert.assertType+'('+assert.expectedValue+', '+assert.actualValue)+')">'+ escapeBadXmlChars(assert.message) +'</failure>\n';
-            output += '  </testcase>\n';
+    // save XML reports to file
+    for (var filename in reports) {
+        if (reports.hasOwnProperty(filename)) {
+            console.log("Writing ../" + filename + " ...");
+            fs.writeFileSync("../" + filename, reports[filename], "utf8");
         }
     }
-
-    output += '</testsuite>\n';
-
-    fs.writeFileSync("../TEST-result.xml", output, "utf8");
 
     console.log("Testing completed");
 }).fail(function(e) { // finally capture a rejection.
     var msg = e.message || e;
     console.error("Error: " + msg);
+    browser.quit();
     return 1;
 }).end();
 
---
+++ test/support/wd.js
@@ -0,0 +1,360 @@
+var http = require("http");
+
+var strip = function strip(str) {
+	var x = [];
+	for(var i = 0; i<str.length; i++) {
+		if (str.charCodeAt(i)) {
+			x.push(str.charAt(i));
+		}
+	}
+	return x.join('');
+}
+
+
+var callbackWithData = function(cb, objKey){
+  return function(res) {
+    res.setEncoding('utf8');
+
+    var data = "";
+    res.on('data', function(chunk) { data += chunk.toString(); });
+    res.on('end', function() {
+      try{
+        var obj = JSON.parse(strip(data));
+      } catch (e) {
+        return cb ('Not JSON response', data)
+      }
+      cb(null, objKey ? obj[objKey] : obj);
+    });
+  }
+
+}
+
+
+
+var webdriver = function(host, port, username, accessKey) {
+  this.sessionID = null;
+  this.options = {
+    host: host || '127.0.0.1'
+    , port: port || 4444
+    , path: '/wd/hub/session'
+    , method: 'POST'
+  };
+  this.desiredCapabilities = {
+    browserName: "firefox"
+    , version: ""
+    , javascriptEnabled: true
+    , platform: "ANY",
+  }
+
+  if (username && accessKey) {
+    var authString = username+":"+accessKey;
+    var buf = new Buffer(authString);
+    this.options['headers'] = {
+      'Authorization': 'Basic '+ buf.toString('base64')
+    }
+    this.desiredCapabilities.platform = "VISTA";
+  }
+
+  this.getOpts = function(over) {
+    var opt = new Object();
+    for (var o in this.options) {
+      opt[o] = this.options[o];
+    }
+    opt['path'] += '/'+this.sessionID;
+    if (over.url) {
+      opt['path'] += over.url;
+    }
+    if (over.method) {
+      opt['method'] = over.method;
+    }
+    return opt;
+  }
+};
+
+webdriver.prototype.init = function(desired, cb) {
+  var _this = this;
+
+  //allow desired ovveride to be left out
+  if (typeof desired == "function") {
+    cb = desired;
+    desired = null;
+  }
+
+  if (desired && desired.browserName) {
+    this.desiredCapabilities.browserName = desired.browserName;
+  }
+  if (desired && desired.version) {
+    this.desiredCapabilities.version = desired.version;
+  }
+  if (desired && desired.javascriptEnabled) {
+    this.desiredCapabilities.javascriptEnabled = desired.javascriptEnabled;
+  }
+  if (desired && desired.platform) {
+    this.desiredCapabilities.platform = desired.platform;
+  }
+  if (desired && desired.name) {
+    this.desiredCapabilities.name = desired.name;
+  }
+
+  var req = http.request(_this.options, function(res) {
+    var data = '';
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+    res.on('end', function() {
+      if (res.headers.location == undefined) {
+        console.log("\x1b[31mError\x1b[0m: The environment you requested was unavailable.\n");
+        console.log("\x1b[33mReason\x1b[0m:\n");
+        console.log(data);
+        console.log("\nFor the available values please consult the WebDriver JSONWireProtocol,");
+        console.log("located at: \x1b[33mhttp://code.google.com/p/selenium/wiki/JsonWireProtocol#/session\x1b[0m");
+        return;
+      }
+      var locationArr = res.headers.location.split("/")
+      _this.sessionID = locationArr[locationArr.length - 1];
+      if (cb) { cb(null, _this.sessionID) }
+    });
+  });
+  req.write(JSON.stringify({desiredCapabilities: _this.desiredCapabilities}));
+  req.end();
+};
+
+webdriver.prototype.close = function(cb) {
+  var _this = this;
+  var req = http.request(
+    _this.getOpts(
+      {url:'/window', method:'DELETE'}
+    ), function(res) {
+      if (cb){ cb(); }
+    });
+
+  req.write("");
+  req.end();
+}
+
+webdriver.prototype.quit = function(cb) {
+  var _this = this;
+  var req = http.request(
+    _this.getOpts(
+      {method:'DELETE'}
+    ), function(res) {
+      if (cb){ cb(); }
+    });
+
+  req.write("");
+  req.end();
+}
+
+webdriver.prototype.eval = function(code, cb) {
+  var _this = this;
+
+  var req = http.request(
+    _this.getOpts({url:'/execute'})
+    , callbackWithData(cb, 'value'));
+
+  req.write(JSON.stringify({script:"return "+code, args:[]}));
+  req.end();
+};
+
+webdriver.prototype.execute = function(code, cb) {
+  var _this = this;
+
+  var req = http.request(
+    _this.getOpts({url:'/execute'})
+    , callbackWithData(cb, 'value'));
+
+  req.write(JSON.stringify({script:code, args:[]}));
+  req.end();
+};
+
+webdriver.prototype.executeAsync = function(code, cb) {
+  var _this = this;
+
+  var req = http.request(
+    _this.getOpts({url:'/execute_async'})
+    , callbackWithData(cb, 'value'));
+
+  req.write(JSON.stringify({script:code, args:[]}));
+  req.end();
+};
+
+webdriver.prototype.get = function(url, cb) {
+  var _this = this;
+
+  var req = http.request(
+    _this.getOpts({url:'/url'}), function(res) {
+      if (cb){ cb(); }
+  });
+
+  req.write(JSON.stringify({"url":url}));
+  req.end();
+};
+
+webdriver.prototype.setWaitTimeout = function(ms, cb) {
+    var _this = this;
+
+    var req = http.request(
+    _this.getOpts({url:'/timeouts/implicit_wait'}), function(res) {
+        if (cb){ cb(); }
+    });
+
+    req.write(JSON.stringify({ms: ms}));
+    req.end();
+};
+
+
+webdriver.prototype.element = function(using, value, cb) {
+    var _this = this;
+
+    var req = http.request(
+      _this.getOpts({url:'/element'}), function(res) {
+        res.setEncoding('utf8');
+
+        var data = "";
+        res.on('data', function(chunk) { data += chunk.toString(); });
+        res.on('end', function() {
+            var obj = JSON.parse(strip(data));
+            if (!obj.value.ELEMENT)
+                cb(data, null);
+            else if (cb){ cb(null, obj.value.ELEMENT); }
+        });
+    });
+
+    req.write(JSON.stringify({
+        using : using,
+        value : value
+    }));
+    req.end();
+};
+
+
+webdriver.prototype.moveTo = function(element, xoffset, yoffset, cb) {
+    var _this = this;
+
+    var req = http.request(
+      _this.getOpts({url:'/moveto'}), function(res) {
+        if (cb){ cb(); }
+    });
+
+    req.write(JSON.stringify({
+        element : element,
+        xoffset : xoffset,
+        yoffset : yoffset
+    }));
+    req.end();
+};
+
+//@todo simulate the scroll event using dispatchEvent and browser.execute
+webdriver.prototype.scroll = function(element, xoffset, yoffset, cb) {
+    var _this = this;
+
+    var req = http.request(
+      _this.getOpts({url:'/moveto'}), function(res) {
+        if (cb){ cb(); }
+    });
+
+    req.write(JSON.stringify({
+        element : element,
+        xoffset : xoffset,
+        yoffset : yoffset
+    }));
+    req.end();
+};
+
+webdriver.prototype.buttonDown = function(cb) {
+    var _this = this;
+
+    var req = http.request(
+    _this.getOpts({url:'/buttondown'}), function(res) {
+        if (cb){ cb(); }
+    });
+
+    req.write(JSON.stringify({}));
+    req.end();
+};
+
+
+webdriver.prototype.buttonUp = function(cb) {
+    var _this = this;
+
+    var req = http.request(
+      _this.getOpts({url:'/buttonup'}), function(res) {
+        if (cb){ cb(); }
+    });
+
+    req.write(JSON.stringify({}));
+    req.end();
+};
+
+//{LEFT = 0, MIDDLE = 1 , RIGHT = 2}
+webdriver.prototype.click = function(button, cb) {
+    var _this = this;
+
+    var req = http.request(
+      _this.getOpts({url:'/click'}), function(res) {
+        if (cb){ cb(); }
+    });
+
+    req.write(JSON.stringify({
+        button: button
+    }));
+    req.end();
+};
+
+webdriver.prototype.doubleclick = function(button, cb) {
+    var _this = this;
+
+    var req = http.request(
+      _this.getOpts({url:'/doubleclick'}), function(res) {
+        if (cb){ cb(); }
+    });
+
+    req.write(JSON.stringify({}));
+    req.end();
+};
+
+//All keys are up at end of command
+webdriver.prototype.type = function(element, keys, cb) {
+    var _this = this;
+
+    if (!keys instanceof Array)
+      keys = [keys]
+
+    var req = http.request(
+      _this.getOpts({url:'/element/' + element + '/value'}), function(res) {
+        if (cb){ cb(); }
+    });
+
+    req.write(JSON.stringify({
+        value : keys
+    }));
+    req.end();
+};
+
+webdriver.prototype.active = function(cb){
+  var req = http.request(
+    this.getOpts({url:'/element/active'}), callbackWithData(function(e, o){
+      cb(null, o['ELEMENT'])
+    }, 'value'));
+
+  req.end();
+}
+
+webdriver.prototype.keyToggle = function(element, keys, cb) {
+    var _this = this;
+
+    var req = http.request(
+      _this.getOpts({url:'/type'}), function(res) {
+        if (cb){ cb(); }
+    });
+
+    req.write(JSON.stringify({
+        value : keys
+    }));
+    req.end();
+};
+
+exports.webdriver = webdriver;
+exports.remote = function(host, port, username, accessKey) {
+  return new webdriver(host, port, username, accessKey);
+}
