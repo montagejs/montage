82aeb7294c22d53a4389b9a838b14dc73e09914f
Stuart Knightley <stuartk@motorola.com>
2012-03-06 13:53:14 -0800

    Disable popup blocking on Chrome

--- test/support/run-tests-remote.js
+++ test/support/run-tests-remote.js
@@ -38,6 +38,9 @@ var browser = wd.remote(WEBDRIVER_HUB_HOST, WEBDRIVER_HUB_PORT);
 // get the browser
 browser.init({
     browserName: BROWSER,
+
+    "chrome.switches": ["--disable-popup-blocking"],
+
     "opera.binary": process.env.OPERA_BINARY,
     "opera.port": 0,
     "opera.profile": null
