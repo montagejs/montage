df07019b0f6dfa0a1cad80ac63359dbc66f40f4f
Simon <bfpv37@motorola.com>
2012-03-14 01:54:10 +0100

    Added Radio
    Same problem with the double types.

---
+++ ui/skeleton/radio.reel/radio.js
@@ -0,0 +1,16 @@
+/* <copyright>
+ This file contains proprietary software owned by Motorola Mobility, Inc.<br/>
+ No rights, expressed or implied, whatsoever to this software are provided by Motorola Mobility, Inc. hereunder.<br/>
+ (c) Copyright 2011 Motorola Mobility, Inc.  All Rights Reserved.
+ </copyright> */
+/*global require,exports */
+var Montage = require("montage").Montage,
+    Component = require("ui/component").Component;    
+
+/**
+ * Radio
+ */
+var Radio = exports.Radio = Montage.create(Component, {
+    
+    
+});
