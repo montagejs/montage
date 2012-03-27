fade39e8cd16f87f0c2339a5cf806e15356d885b
Pierre Frisch <pierre.frisch@motorola.com>
2012-03-27 09:16:19 -0700

    Rework the managed objects

--- data/objectproperty.js
+++ data/objectproperty.js
@@ -8,7 +8,6 @@
  @requires montage/data/pledge
  @requires montage/core/core
  @requires montage/core/exception
- @requires montage/core/event/mutable-event
  @requires montage/core/logger
  */
 var Montage = require("montage").Montage;
@@ -31,10 +30,10 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @function
      @returns itself
      */
-    init: {
-        serializable: false,
-        enumerable: false,
-        value: function() {
+    init:{
+        serializable:false,
+        enumerable:false,
+        value:function () {
             return this;
         }
     },
@@ -46,9 +45,9 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @param {Property} prototype TODO
      @param {Object} blueprint TODO
      */
-    apply: {
-        value: function(prototype, blueprint) {
-            if (! prototype.hasOwnProperty("blueprint")) {
+    apply:{
+        value:function (prototype, blueprint) {
+            if (!prototype.hasOwnProperty("blueprint")) {
                 var info;
                 info = Montage.getInfoForObject(prototype);
                 if (info != null && info.isInstance === false) {
@@ -71,8 +70,8 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @param {Property} prototype TODO
      @param {Object} blueprint TODO
      */
-    applyWithBlueprint: {
-        value: function(prototype, blueprint) {
+    applyWithBlueprint:{
+        value:function (prototype, blueprint) {
             if (blueprint != null) {
                 this.addProperties(prototype, blueprint);
                 if (blueprint.parent !== null) {
@@ -87,8 +86,8 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @param {Property} prototype TODO
      @param {Object} blueprint TODO
      */
-    addProperties: {
-        value: function(prototype, blueprint) {
+    addProperties:{
+        value:function (prototype, blueprint) {
             //for loop over attributes
             var i, attribute;
             for (i = 0; attribute = blueprint.attributes[i]; i++) {
@@ -101,18 +100,18 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
                 }
             }
 
-            Montage.defineProperty(prototype, "context", { serializable: false, enumerable: true, value: null });
-            Montage.defineProperty(prototype, "_objectId", { serializable: true, enumerable: false, value: null });
+            Montage.defineProperty(prototype, "context", { serializable:false, enumerable:true, value:null });
+            Montage.defineProperty(prototype, "_objectId", { serializable:true, enumerable:false, value:null });
             Montage.defineProperty(prototype, "objectId", {
-                enumerable: true,
-                serializable: false,
-                get: function() {
+                enumerable:true,
+                serializable:false,
+                get:function () {
                     if (this._objectId === null) {
                         this._objectId = this.blueprint.objectId$Implementation;
                     }
                     return this._objectId;
                 },
-                set: function(value) {
+                set:function (value) {
                     if (value !== null) {
                         this._objectId = value;
                     } else {
@@ -120,22 +119,22 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
                     }
                 }
             });
-            Montage.defineProperty(prototype, "_blueprint", { serializable: false, enumerable: false, value: blueprint });
-            Montage.defineProperty(prototype, "blueprint", { enumerable: false, serializable: false, get: function() {
+            Montage.defineProperty(prototype, "_blueprint", { serializable:false, enumerable:false, value:blueprint });
+            Montage.defineProperty(prototype, "blueprint", { enumerable:false, serializable:false, get:function () {
                 return this._blueprint;
             }});
-            Montage.defineProperty(prototype, "isPledge", { serializable: false, enumerable: true, value: false });
-            Montage.defineProperty(prototype, "withProperties", { serializable: false, enumerable: false, value: function() {
+            Montage.defineProperty(prototype, "isPledge", { serializable:false, enumerable:true, value:false });
+            Montage.defineProperty(prototype, "withProperties", { serializable:false, enumerable:false, value:function () {
                 return null;
             }});
-            Montage.defineProperty(prototype, "willRead", { serializable: false, enumerable: false, value: this.willRead });
-            Montage.defineProperty(prototype, "willModify", { serializable: false, enumerable: false, value: this.willModify });
+            Montage.defineProperty(prototype, "willRead", { serializable:false, enumerable:false, value:this.willRead });
+            Montage.defineProperty(prototype, "willModify", { serializable:false, enumerable:false, value:this.willModify });
             // Enable access to the 'inherited' get method for easy override.
-            Montage.defineProperty(prototype, "blueprintGet", { serializable: false, enumerable: false, value: blueprint.blueprintGet});
+            Montage.defineProperty(prototype, "blueprintGet", { serializable:false, enumerable:false, value:blueprint.blueprintGet});
             // Enable access to the 'inherited' set method for easy override.
-            Montage.defineProperty(prototype, "blueprintSet", { serializable: false, enumerable: false, value: blueprint.blueprintSet});
+            Montage.defineProperty(prototype, "blueprintSet", { serializable:false, enumerable:false, value:blueprint.blueprintSet});
             // Provide a storage property for any state the access layer need to store in teh object. This would typically be a database snapshot reference.
-            Montage.defineProperty(prototype, "_opaqueAccessState", { serializable: false, enumerable: false, value: null});
+            Montage.defineProperty(prototype, "_opaqueAccessState", { serializable:false, enumerable:false, value:null});
         }
     },
     /**
@@ -144,8 +143,8 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @param {Property} prototype TODO
      @param {Object} attribute TODO
      */
-    addProperty: {
-        value: function(prototype, attribute) {
+    addProperty:{
+        value:function (prototype, attribute) {
             this.addPropertyStorage(prototype, attribute);
             this.addPropertyDefinition(prototype, attribute);
             this.addPropertyStoredValue(prototype, attribute);
@@ -157,28 +156,30 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @param {Property} prototype TODO
      @param {Object} attribute TODO
      */
-    addPropertyStorage: {
-        value: function(prototype, attribute) {
+    addPropertyStorage:{
+        value:function (prototype, attribute) {
             var storageKey = "_" + attribute.name,
                 storageDefinition = null;
-            if (! prototype.hasOwnProperty(storageKey)) {
+            if (!prototype.hasOwnProperty(storageKey)) {
                 if (attribute.isToMany) {
                     storageDefinition = {
-                        value: [],
-                        enumerable: false,
-                        serializable: true,
-                        distinct: true
+                        value:[],
+                        enumerable:false,
+                        serializable:true,
+                        distinct:true
                     };
                 } else {
                     storageDefinition = {
-                        value: null,
-                        enumerable: false,
-                        serializable: true
+                        value:null,
+                        enumerable:false,
+                        serializable:true
                     };
                 }
                 Montage.defineProperty(prototype, storageKey, storageDefinition);
             } else {
-                // We have an issue here. The developer should not override the storage value.
+                if (logger.isError) {
+                    logger.error("We have an issue here. The developer should not override the storage value for " + storageKey + ".");
+                }
             }
         }
     },
@@ -189,26 +190,28 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @param {Property} prototype TODO
      @param {Object} attribute TODO
      */
-    addPropertyDefinition: {
-        value : function(prototype, attribute) {
+    addPropertyDefinition:{
+        value:function (prototype, attribute) {
             var propertyKey = attribute.name,
                 propertyDefinition = null;
-            if (! prototype.hasOwnProperty(propertyKey)) {
+            if (!prototype.hasOwnProperty(propertyKey)) {
                 propertyDefinition = {
-                    get: function() {
+                    get:function () {
                         return this.blueprintGet(propertyKey);
                     },
-                    enumerable: true,
-                    serializable: false
+                    enumerable:true,
+                    serializable:false
                 };
-                if (! attribute.readOnly) {
-                    propertyDefinition.set = function(value) {
+                if (!attribute.readOnly) {
+                    propertyDefinition.set = function (value) {
                         return this.blueprintSet(propertyKey, value);
                     };
                 }
                 Montage.defineProperty(prototype, propertyKey, propertyDefinition);
             } else {
-                // The developer has already created the property method do nothing.
+                if (logger.isDebug) {
+                    logger.debug("The developer has already created the property " + propertyKey + " method do nothing.");
+                }
             }
         }
     },
@@ -219,28 +222,30 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @param {Property} prototype TODO
      @param {Object} attribute TODO
      */
-    addPropertyStoredValue: {
-        value: function(prototype, attribute) {
+    addPropertyStoredValue:{
+        value:function (prototype, attribute) {
             var storedValueKey = attribute.name + "$Storage",
                 storedValueDefinition = null;
-            if (! prototype.hasOwnProperty(storedValueKey)) {
+            if (!prototype.hasOwnProperty(storedValueKey)) {
                 if (attribute.isToMany) {
                     storedValueDefinition = {
-                        value: [],
-                        enumerable: false,
-                        serializable: false,
-                        distinct: true
+                        value:[],
+                        enumerable:false,
+                        serializable:false,
+                        distinct:true
                     }
                 } else {
                     storedValueDefinition = {
-                        value: null,
-                        enumerable: false,
-                        serializable: false
+                        value:null,
+                        enumerable:false,
+                        serializable:false
                     }
                 }
                 Montage.defineProperty(prototype, storedValueKey, storedValueDefinition);
             } else {
-                // We have an issue here. The developer should not override the stored value.
+                if (logger.isError) {
+                    logger.error("We have an issue here. The developer should not override the stored value for " + storedValueKey + ".");
+                }
             }
         }
     },
@@ -251,8 +256,8 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @param {Property} prototype TODO
      @param {Object} attribute relationship to add
      */
-    addRelationship: {
-        value: function(prototype, attribute) {
+    addRelationship:{
+        value:function (prototype, attribute) {
             this.addPropertyStorage(prototype, attribute);
             this.addRelationshipDefinition(prototype, attribute);
             this.addPropertyStoredValue(prototype, attribute);
@@ -266,18 +271,39 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @param {Property} prototype TODO
      @param {Object} attribute TODO
      */
-    addRelationshipDefinition: {
-        value : function(prototype, attribute) {
+    addRelationshipDefinition:{
+        value:function (prototype, attribute) {
             var relationshipKey = attribute.name.toCapitalized();
-            Montage.defineProperty(prototype, "addTo" + relationshipKey, { serializable: false, enumerable: false, value: function() {
-                return null;
-            }});
-            Montage.defineProperty(prototype, "removeFrom" + relationshipKey, { serializable: false, enumerable: false, value: function() {
-                return null;
-            }});
-            Montage.defineProperty(prototype, "clear" + relationshipKey, { serializable: false, enumerable: false, value: function() {
-                return null;
-            }});
+            var key = "addTo" + relationshipKey;
+            if (!prototype.hasOwnProperty(key)) {
+                Montage.defineProperty(prototype, key, { serializable:false, enumerable:false, value:function () {
+                    return null;
+                }});
+            } else {
+                if (logger.isError) {
+                    logger.error("We have an issue here. The developer should not override the method " + key + ".");
+                }
+            }
+            key = "removeFrom" + relationshipKey;
+            if (!prototype.hasOwnProperty(key)) {
+                Montage.defineProperty(prototype, key, { serializable:false, enumerable:false, value:function () {
+                    return null;
+                }});
+            } else {
+                if (logger.isError) {
+                    logger.error("We have an issue here. The developer should not override the method " + key + ".");
+                }
+            }
+            key = "clear" + relationshipKey;
+            if (!prototype.hasOwnProperty(key)) {
+                Montage.defineProperty(prototype, key, { serializable:false, enumerable:false, value:function () {
+                    return null;
+                }});
+            } else {
+                if (logger.isError) {
+                    logger.error("We have an issue here. The developer should not override the method " + key + ".");
+                }
+            }
         }
     },
 
@@ -287,8 +313,8 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @param {Property} prototype TODO
      @param {Object} attribute TODO
      */
-    addDerivedProperty: {
-        value: function(prototype, attribute) {
+    addDerivedProperty:{
+        value:function (prototype, attribute) {
         }
     },
 
@@ -297,8 +323,8 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @function
      @param {Object} attribute TODO
      */
-    willRead: {
-        value: function(attribute) {
+    willRead:{
+        value:function (attribute) {
             var storageKey = "_" + attribute.name;
             if (typeof this[storageKey] !== 'undefined') {
                 // the property is already resolved nothing to do.
@@ -315,8 +341,8 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @param {Object} attribute TODO
      @param {Property} value TODO
      */
-    willModify: {
-        value: function(attribute, value) {
+    willModify:{
+        value:function (attribute, value) {
             var storageKey = "_" + attribute.name;
             var previousValue = this[storageKey];
             if ((typeof previousValue === 'undefined') || (previousValue !== value)) {
@@ -334,8 +360,8 @@ var ObjectProperty = exports.ObjectProperty = Montage.create(Montage, /** @lends
      @function
      @returns object
      */
-    manager: {
-        get: function() {
+    manager:{
+        get:function () {
             if (_objectPropertyManager === null) {
                 _objectPropertyManager = Object.freeze(ObjectProperty.create().init());
             }
