a2aaa09faeedb912a132d7b3e0e8eb58851f18fa
Stuart Knightley <stuartk@motorola.com>
2012-04-02 10:40:41 -0700

    Exit editing HotText on pressing enter

--- ui/hot-text.reel/hot-text.js
+++ ui/hot-text.reel/hot-text.js
@@ -248,6 +248,10 @@ var HotText = exports.HotText = Montage.create(Component, {
                 // down
                 this.value -= this.stepSize;
                 this.needsDraw = true;
+            } else if (event.keyCode === 13) {
+                this._isEditing = false;
+                this.convertedValue = this._inputElement.value;
+                this.needsDraw = true;
             }
         }
     },
