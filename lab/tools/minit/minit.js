1e3aefc85d45974d0c72eaf35fc953cbe4e942d4
François Frisch <francois@motorola.com>
2012-04-18 16:29:28 -0700

    Making minit executable

--- lab/tools/minit/minit.js
+++ lab/tools/minit/minit.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /* <copyright>
  This file contains proprietary software owned by Motorola Mobility, Inc.<br/>
  No rights, expressed or implied, whatsoever to this software are provided by Motorola Mobility, Inc. hereunder.<br/>
