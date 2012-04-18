2e3b33e112018835ff62b373e6c21025ffb4da16
François Frisch <francois@motorola.com>
2012-04-18 14:47:36 -0700

    Adding minit
    Uses a duplicates a set of files/directories based on a template.
    By default it uses a Mustache format.
    
    Usage:
    node minit.js template_name name [<variables>]

---
+++ lab/tools/minit/minit.js
@@ -0,0 +1,50 @@
+/* <copyright>
+ This file contains proprietary software owned by Motorola Mobility, Inc.<br/>
+ No rights, expressed or implied, whatsoever to this software are provided by Motorola Mobility, Inc. hereunder.<br/>
+ (c) Copyright 2012 Motorola Mobility, Inc.  All Rights Reserved.
+ </copyright> */
+
+var args = process.argv.slice(3);
+var options = {};
+options.templateName = process.argv[2];
+
+do {
+    var arg = args[0],
+        argumentConsumed = false;
+
+    switch (arg) {
+        case "--dry-run":
+        argumentConsumed = true;
+        options.dryRun = true;
+        break;
+    }
+
+    if (argumentConsumed) {
+        args.shift();
+    }
+} while (argumentConsumed);
+
+
+if (args.length === 0) {
+    usage();
+    process.exit();
+}
+
+options.minitHome = process.argv[1].split("/").slice(0, -1).join("/") + "/";
+
+var fs = require("fs");
+
+function usage() {
+    console.log("Missing arguments.");
+    console.log("Usage: " + process.argv[1] + " [--dry-run] <filename.html> | <directory>");
+}
+var templatePath = options.minitHome + "templates/" + options.templateName
+var Template = require(templatePath).Template;
+var templateStats = fs.statSync(templatePath);
+if (templateStats.isDirectory()) {
+    var aTemplate = Template.newWithNameAndOptions(templatePath, options);
+    aTemplate.process(args);
+} else {
+    console.log("Invalid arguments.");
+    console.log("Missing template directory: " + templatePath);
+}
---
+++ lab/tools/minit/templates/test.js
@@ -0,0 +1,35 @@
+/* <copyright>
+ This file contains proprietary software owned by Motorola Mobility, Inc.<br/>
+ No rights, expressed or implied, whatsoever to this software are provided by Motorola Mobility, Inc. hereunder.<br/>
+ (c) Copyright 2012 Motorola Mobility, Inc.  All Rights Reserved.
+ </copyright> */
+
+var TemplateBase = require("../lib/template-base.js").TemplateBase;
+var childProcess = require('child_process');
+
+exports.Template = Object.create(TemplateBase, {
+
+    processArguments: {
+        value: function(args) {
+            TemplateBase.processArguments.apply(this, arguments);
+            this.variables.title = args[1];
+            if (!this.variables.title) {
+                this.variables.title = this.variables.name.replace(/(?:^|-)([^-])/g, function(_, g1) { return g1.toUpperCase() });
+            }
+        }
+    },
+
+    destination: {
+        value: "../../../test/ui/"
+    },
+
+    finish: {
+        value: function() {
+            TemplateBase.finish.apply(this, arguments);
+            console.log("Direct Link:");
+            console.log("http://localhost:8081/m-js/test/run.html?spec=ui%2F" + this.variables.name + "-spec");
+            childProcess.exec("open http://localhost:8081/m-js/test/run.html?spec=ui%2F" + this.variables.name + "-spec");
+        }
+    }
+
+});
