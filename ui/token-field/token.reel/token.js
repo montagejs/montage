97044cde5cca9b0b2a952ea4daae5d7a8449f0ba
Kishore Subramanian <kishore.subramanian@motorola.com>
2012-04-26 16:22:12 -0700

    Add a CSS class for ad-hoc tokens in TokenField with adhoc support.

--- ui/token-field/token.reel/token.js
+++ ui/token-field/token.reel/token.js
@@ -18,11 +18,13 @@ exports.Token = Montage.create(Component, {
             return this._value;
         },
         set: function(aValue) {
+            this._adHoc = false;
             if(aValue) {
                this._value = aValue;
                if(this.textPropertyPath) {
                    if(typeof aValue[this.textPropertyPath] == 'undefined' && this.allowAdHocValues) {
                        this.text = aValue;
+                       this._adHoc = true;
                    } else {
                        this.text = this.value[this.textPropertyPath];
                    }
@@ -36,6 +38,17 @@ exports.Token = Montage.create(Component, {
     textPropertyPath: {value: null},
 
     tokensController: {value: null},
+
+    __adHoc: {value: null},
+    _adHoc: {
+        get: function() {
+            return this.__adHoc;
+        },
+        set: function(value) {
+            this.__adHoc = value;
+            this.needsDraw = true;
+        }
+    },
 
     _pressComposer: {
         enumberable: false,
@@ -71,7 +84,7 @@ exports.Token = Montage.create(Component, {
 
     draw: {
         value: function() {
-
+            this.element.classList[this._adHoc ? 'add' : 'remove']('montage-token-adhoc');
         }
     },
 
