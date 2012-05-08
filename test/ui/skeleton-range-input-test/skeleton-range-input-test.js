65777180ec37a88367f2fddb13b409d4dd4409a3
Kishore Subramanian <kishore.subramanian@motorola.com>
2012-05-08 11:51:43 -0700

    Unit tests for skeleton/range-input component. Related to gh-534.

---
+++ test/ui/skeleton-range-input-test/skeleton-range-input-test.js
@@ -0,0 +1,8 @@
+/* <copyright>
+ This file contains proprietary software owned by Motorola Mobility, Inc.<br/>
+ No rights, expressed or implied, whatsoever to this software are provided by Motorola Mobility, Inc. hereunder.<br/>
+ (c) Copyright 2011 Motorola Mobility, Inc.  All Rights Reserved.
+ </copyright> */
+var Montage = require("montage").Montage;
+
+var SkeletonRangeInputTest = exports.SkeletonRangeInputTest = Montage.create(Montage, {});
