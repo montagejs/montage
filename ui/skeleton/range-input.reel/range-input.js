3e36d89ed541a268f9d947ca775ef5dce9df40e3
Kishore Subramanian <kishore.subramanian@motorola.com>
2012-05-08 11:07:23 -0700

    Refactored the fix for gh-534 based on code review.

--- ui/skeleton/range-input.reel/range-input.js
+++ ui/skeleton/range-input.reel/range-input.js
@@ -63,10 +63,8 @@ var RangeInput = exports.RangeInput = Montage.create(Component, {
          }
      },
 
-
-   _sliding: {value: false},
-
    percent: {value: null},
+
    _valueSyncedWithPosition: {value: null},
    _value: {value: null},
     value: {
@@ -87,23 +85,21 @@ var RangeInput = exports.RangeInput = Montage.create(Component, {
    },
 
     // private
-    sliderEl: {value: null, enumerable: false},
-    handleEl: {value: null, enumerable: false},
-    sliderLeft: {value: null, enumerable: false},
-    sliderWidth: {value: null, enumerable: false},
-    minX: {value: null, enumerable: false},
-    maxX: {value: null, enumerable: false},
+    _handleEl: {value: null, enumerable: false},
+    _sliderLeft: {value: null, enumerable: false},
+    _sliderWidth: {value: null, enumerable: false},
 
-    _positionX: {value: null},
-    positionX: {
+
+    __positionX: {value: null},
+    _positionX: {
         enumerable: false,
         get: function() {
-            return this._positionX;
+            return this.__positionX;
         },
         set: function(value, fromValue) {
 
             if(value !== null && !isNaN(value)) {
-                this._positionX = value;
+                this.__positionX = value;
                 if(!fromValue) {
                     this._calculateValueFromPosition();
                     this._valueSyncedWithPosition = true;
@@ -116,8 +112,8 @@ var RangeInput = exports.RangeInput = Montage.create(Component, {
 
     _calculateValueFromPosition: {
         value: function() {
-            if(this.sliderWidth > 0) {
-                var percent = this.percent = (this.positionX / this.sliderWidth) * 100;
+            if(this._sliderWidth > 0) {
+                var percent = this.percent = (this._positionX / this._sliderWidth) * 100;
                 var value = (this.min + ((percent/100) * (this.max - this.min)));
                 Object.getPropertyDescriptor(this, "value").set.call(this, value, true);
             }
@@ -128,12 +124,13 @@ var RangeInput = exports.RangeInput = Montage.create(Component, {
     _calculatePositionFromValue: {
         value: function() {
             // unless the element is ready, we cannot position the handle
-            if(this.sliderWidth) {
+            if(this._sliderWidth) {
                 var percent, value = this.value;
                 var range = (this.max - this.min);
                 percent = ((this.value-this.min)/range) * 100;
-                var positionX = (percent/100)*this.sliderWidth;
-                Object.getPropertyDescriptor(this, "positionX").set.call(this, positionX, true);
+                var positionX = (percent/100)*this._sliderWidth;
+                Object.getPropertyDescriptor(this, "_positionX").set.call(this, positionX, true);
+
                 this.percent = percent;
                 this._valueSyncedWithPosition = true;
             } else {
@@ -144,25 +141,19 @@ var RangeInput = exports.RangeInput = Montage.create(Component, {
 
     deserializedFromTemplate: {
         value: function() {
-
             // read initial values from the input type=range
-
             this.min = this.min || this.element.getAttribute('min') || 0;
             this.max = this.max || this.element.getAttribute('max') || 100;
             this.step = this.step || this.element.getAttribute('step') || 1;
             this.value = this.value || this.element.getAttribute('value') || 0;
-
-
         }
     },
 
     prepareForDraw: {
         value: function() {
-            this.minX = this.sliderLeft = this.element.offsetLeft;
-            this.sliderWidth =  (this.width || RangeInput.DEFAULT_WIDTH); //this.element.offsetWidth || 300;
-            this.element.style.width = (this.sliderWidth + RangeInput.HANDLE_ADJUST) + 'px';
-
-            this.maxX = this.sliderLeft + this.sliderWidth;
+            this._sliderLeft = this.element.offsetLeft;
+            this._sliderWidth =  (this.width || RangeInput.DEFAULT_WIDTH); //this.element.offsetWidth || 300;
+            this.element.style.width = (this._sliderWidth + RangeInput.HANDLE_ADJUST) + 'px';
 
             if(!this._valueSyncedWithPosition) {
                 this._calculatePositionFromValue();
@@ -170,56 +161,72 @@ var RangeInput = exports.RangeInput = Montage.create(Component, {
         }
     },
 
-    // @todo: Without prepareForActivationEvents, the translateComposer does not work
+    // @todo: Without prepareForActivationEvents, the _translateComposer does not work
     prepareForActivationEvents: {
         value: function() {
-            this.translateComposer.addEventListener('translateStart', this, false);
-            this.translateComposer.addEventListener('translateEnd', this, false);
+            this._translateComposer.addEventListener('translateStart', this, false);
+            this._translateComposer.addEventListener('translateEnd', this, false);
+
+            this._addEventListeners();
+
+        }
+    },
+
+    _addEventListeners: {
+        value: function() {
             if(window.Touch) {
                 this.element.addEventListener('touchend', this, false);
             } else {
                 this.element.addEventListener('mouseup', this, false);
             }
+        }
+    },
 
+    _removeEventListeners: {
+        value: function() {
+            if(window.Touch) {
+                this.element.removeEventListener('touchend', this, false);
+            } else {
+                this.element.removeEventListener('mouseup', this, false);
+            }
         }
     },
 
     handleTranslateStart: {
         value: function(e) {
+            this._removeEventListeners();
             this._valueSyncedWithPosition = false;
-            this._sliding = true;
         }
     },
 
     handleTranslateEnd: {
         value: function(e) {
-            this._sliding = false;
+            this._addEventListeners();
         }
     },
 
     // handle user clicking the slider scale directly instead of moving the knob
     _handleClick: {
-        value: function(e) {
-            var position = this.translateComposer._pointerX;
-            var relativePosition = (position - this.sliderLeft);
-            if(relativePosition > 0) {
-                var positionX = (position - (this.sliderLeft + 2*RangeInput.HANDLE_ADJUST));
+        value: function(position) {
+            var relativePosition = (position - this._sliderLeft);
+            if(relativePosition > 0 && relativePosition < (this._sliderLeft + this._sliderWidth + RangeInput.HANDLE_ADJUST)) {
+                var positionX = (position - (this._sliderLeft + 2*RangeInput.HANDLE_ADJUST));
                 if(positionX < 0) {
                     positionX = 0;
                 }
-                this.positionX = positionX;
+                this._positionX = positionX;
             }
         }
     },
 
     handleMouseup: {
         value: function(e) {
-            this._handleClick(e);
+            this._handleClick(e.clientX);
         }
     },
     handleTouchend: {
-        value: function(e) {
-            this._handleClick(e);
+        value: function(event) {
+            this._handleClick(event.changedTouches[0].clientX);
         }
     },
 
@@ -235,18 +242,18 @@ var RangeInput = exports.RangeInput = Montage.create(Component, {
 
     draw: {
         value: function() {
-            var el = this.handleEl;
+            var el = this._handleEl;
 
             if(el.style.webkitTransform != null) {
                 // detection for webkitTransform to use Hardware acceleration where available
-                el.style.webkitTransform = 'translate(' + this.positionX + 'px)';
+                el.style.webkitTransform = 'translate(' + this._positionX + 'px)';
             } else if(el.style.MozTransform != null) {
-                el.style.MozTransform = 'translate(' + this.positionX + 'px)';
+                el.style.MozTransform = 'translate(' + this._positionX + 'px)';
             } else if(el.style.transform != null) {
-                el.style.transform = 'translate(' + this.positionX + 'px)';
+                el.style.transform = 'translate(' + this._positionX + 'px)';
             } else {
                 // fallback
-                el.style['left'] = this.positionX + 'px';
+                el.style['left'] = this._positionX + 'px';
             }
         }
     }
