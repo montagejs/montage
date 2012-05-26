7bf60a67b432c50faf492fced69cae51afadf7a6
Kris Kowal <kris.kowal@motorola.com>
2012-05-25 17:34:24 -0700

    Review shim and extras and their documentation
    Add
    
    -   Object.isObject
    -   Object.has
    -   Object.get
    -   Object.set
    -   Object.getset
    -   Object.forEach
    -   Object.is
    -   Object.clone
    -   Object.deepClone
    -   Object#clone
    -   Object#deepClone
    -   Array#find
    -   Array#findLast
    -   Array#clone
    -   Array#deepClone
    -   Date#clone
    -   Date#deepClone
    
    Refactor
    
    -   Using Object.equals for array elements in Array#equals
    -   Array#has uses #find instead of #indexOf
    -   Array#add accepts `equals` argument and forwards to #has
    -   Array#delete uses #find instead of #indexOf and forwards `equals`
    -   Array#unique defers to #add to ensure uniqueness and forwards
        `equals`
    -   Array#flatten is now sufficiently generic that it only needs
        `reduce` on itself and `forEach` on inner arrays.
    -   Object.equals can now recognize equivalent boxed and unboxed values
    -   Object.compare can now recognize equivalent boxed and unboxed values
    -   Throw exceptions in Array#get and #set if the index is not a number.
    
    General tasks
    
    -   Verify documentation completeness and accuracy with JSDoc
    -   All shims and extras are writable and configurable.
    -   Build common string table

--- core/shim/object.js
+++ core/shim/object.js
@@ -1,8 +1,29 @@
+/* <copyright>
+ This file contains proprietary software owned by Motorola Mobility, Inc.<br/>
+ No rights, expressed or implied, whatsoever to this software are provided by Motorola Mobility, Inc. hereunder.<br/>
+ (c) Copyright 2011 Motorola Mobility, Inc.  All Rights Reserved.
+ </copyright> */
+/**
+    Defines standardized shims for the intrinsic <code>Object</code>.
+    @see [Object class]{@link external:Object}
+    @module montage/core/shim/object
+*/
 
 /**
- @external Object
- */
+    @external Object
+*/
 
+/**
+    Creates a new object that inherits prototypically directly from a given
+    prototype, optionally defining some properties.
+    @function external:Object.create
+    @param {Object} prototype the prototype to inherit, or
+    <code>null</code> for no prototype, which makes "__proto__" the only
+    special property name.
+    @param {Object} descriptor a property descriptor
+    @returns a new object inheriting from the given prototype and having
+    the given property descriptor.
+*/
 if (!Object.create) {
     Object._creator = function _ObjectCreator() {
         this.__proto__ = _ObjectCreator.prototype;
@@ -18,19 +39,28 @@ if (!Object.create) {
     };
 }
 
+// These are used in montage.js to ascertain whether we can annotate
+// objects with montage metadata.
+
+// TODO documentation
 if (!Object.isSealed) {
     Object.defineProperty(Object, "isSealed", {
         value: function() {
             return false;
-        }
+        },
+        writable: true,
+        configurable: true
     });
 }
 
+// TODO documentation
 if (!Object.seal) {
     Object.defineProperty(Object, "seal", {
         value: function(object) {
             return object;
-        }
+        },
+        writable: true,
+        configurable: true
     });
 }
 
