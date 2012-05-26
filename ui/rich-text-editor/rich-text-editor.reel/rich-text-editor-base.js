e949c8e31c7b6b257a839347664de85e5d3779d8
Jean-Francois Ducarroz <ducarroz@motorola.com>
2012-05-26 08:28:33 -0700

    fix pull request comment: use content instead of this.originalContent

--- ui/rich-text-editor/rich-text-editor.reel/rich-text-editor-base.js
+++ ui/rich-text-editor/rich-text-editor.reel/rich-text-editor-base.js
@@ -593,7 +593,7 @@ exports.RichTextEditorBase = Montage.create(Component,/** @lends module:"montage
                         editorInnerElement.appendChild(contents);
                         contentChanged = true;
                     } else {
-                        for (i = 0; this.originalContent && (content = contents[i]); i++) {
+                        for (i = 0; contents && (content = contents[i]); i++) {
                             editorInnerElement.appendChild(content);
                             contentChanged = true;
                         }
