50155d9711ee54f3e61665856231c40e909bd005
Stuart Knightley <stuartk@motorola.com>
2012-06-08 11:26:36 -0700

    Update year in copyright notice

--- core/converter/currency-converter.js
+++ core/converter/currency-converter.js
@@ -1,7 +1,7 @@
 /* <copyright>
  This file contains proprietary software owned by Motorola Mobility, Inc.<br/>
  No rights, expressed or implied, whatsoever to this software are provided by Motorola Mobility, Inc. hereunder.<br/>
- (c) Copyright 2011 Motorola Mobility, Inc.  All Rights Reserved.
+ (c) Copyright 2012 Motorola Mobility, Inc.  All Rights Reserved.
  </copyright> */
 /**
 	@module montage/core/converter/currency-converter
