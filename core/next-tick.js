2da0b614b1ec4b725de489871cb278441ddd1baf
Tim Statler <tim.statler@motorola.com>
2012-06-21 14:39:45 -0700

    Fixed typo in last commit.

--- core/next-tick.js
+++ core/next-tick.js
@@ -31,7 +31,7 @@ if (typeof process !== "undefined") {
 // Browser implementation: based on MessageChannel, setImmediate, or setTimeout
 } else {
 
-    // queue of tasks implemented as a sin`y linked list with a head node
+    // queue of tasks implemented as a singly linked list with a head node
     var head = {}, tail = head;
     // whether a task is pending is represented by the existence of head.next
 
