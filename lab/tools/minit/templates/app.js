3860ad3d4d06da75b147a663a3b176e4dde0d6be
Stuart Knightley <stuartk@motorola.com>
2012-06-25 09:50:08 -0700

    Add basic app template to minit

---
+++ lab/tools/minit/templates/app.js
@@ -0,0 +1,12 @@
+/* <copyright>
+ This file contains proprietary software owned by Motorola Mobility, Inc.<br/>
+ No rights, expressed or implied, whatsoever to this software are provided by Motorola Mobility, Inc. hereunder.<br/>
+ (c) Copyright 2012 Motorola Mobility, Inc.  All Rights Reserved.
+ </copyright> */
+
+var TemplateBase = require("../lib/template-base.js").TemplateBase;
+    path = require("path");
+
+exports.Template = Object.create(TemplateBase, {
+
+});
---
+++ lab/tools/minit/templates/app/__name__/ui/main.reel/main.js
@@ -0,0 +1,24 @@
+/**
+    @module "ui/main.reel"
+    @requires montage
+    @requires montage/ui/component
+*/
+var Montage = require("montage").Montage,
+    Component = require("ui/component").Component;
+
+/**
+    Description TODO
+    @class module:"ui/main.reel".Main
+    @extends module:ui/component.Component
+*/
+exports.Main = Montage.create(Component, /** @lends module:"ui/main.reel".Main# */ {
+
+    hasTemplate: {
+        value: true
+    },
+
+    thing: {
+        value: "World"
+    }
+
+});
