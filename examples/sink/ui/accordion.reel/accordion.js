f65428992f5883b68c2b0a92d4dd49f18ba951bc
Kishore Subramanian <kishore.subramanian@motorola.com>
2012-06-29 14:57:39 -0700

    RichText Editor in the Kitchen Sink (gh-763)

--- examples/sink/ui/accordion.reel/accordion.js
+++ examples/sink/ui/accordion.reel/accordion.js
@@ -9,37 +9,37 @@ var Montage = require("montage/core/core").Montage,
 
 // similar to jquery.closest. Find the closest ancestor of el matching the type
 var findClosestOfType = function(el, type, context, clazz) {
-	if(el.tagName.toLowerCase() === type) {
-		return el;
-	}
-	var found = false;
-	while(el) {
-		el = el.parentNode;
-		if(el.tagName && el.tagName.toLowerCase() === type) {
-		    if(clazz) {
-		        if(el.classList.contains(clazz)) {
-		            found = true;
-		            break;
-		        }
-		    } else {
-		        found = true;
-		        break;
-		    }						
-		}
-		if(!el || !el.ownerDocument || el === context || el.nodeType === 11) {
-			break;
-		}
-	}
-	if(found) {	   
-	    return el;		
-	}
-	return null;
+    if(el.tagName.toLowerCase() === type) {
+        return el;
+    }
+    var found = false;
+    while(el) {
+        el = el.parentNode;
+        if(el.tagName && el.tagName.toLowerCase() === type) {
+            if(clazz) {
+                if(el.classList.contains(clazz)) {
+                    found = true;
+                    break;
+                }
+            } else {
+                found = true;
+                break;
+            }
+        }
+        if(!el || !el.ownerDocument || el === context || el.nodeType === 11) {
+            break;
+        }
+    }
+    if(found) {
+        return el;
+    }
+    return null;
 
 };
 
 exports.Accordion = Montage.create(Component, {
 
-    
+
     prepareForDraw: {
         value: function() {
             if(window.Touch) {
@@ -49,34 +49,21 @@ exports.Accordion = Montage.create(Component, {
             }
         }
     },
-    
-    handleNavItemClicked: {
-        value: function(href) {
-            console.log("nav item clicked = " + href);
-            var hash = href.substring(href.lastIndexOf('#')+1);
-            this.sandbox.selectedItem = hash;
-        }
-    },
 
     handleEvent: {
         value: function(event) {
             event.preventDefault();
             var target = event.target;
-            var $li = findClosestOfType(target, 'li', this.element);
 
-            if($li) {
-                var $heading = findClosestOfType(target, 'div', this.element, "accordion-heading");
-                if($heading) {
-                    // user clicked on the header
-                    var a = $li.querySelector('a');
-                    if(a) {
-                        var $content = $li.querySelector('div.accordion-inner');
-                        $content.classList.toggle('montage-hidden');                    
-                    }
-                    
-                } 
-                
-                
+            var classList = target.classList;
+            if(classList.contains('accordion-toggle') || classList.contains('accordion-heading') || classList.contains('accordion-group')) {
+                var $li = findClosestOfType(target, 'li', this.element);
+
+                var a = $li.querySelector('a');
+                if(a) {
+                    var $content = $li.querySelector('div.accordion-inner');
+                    $content.classList.toggle('montage-hidden');
+                }
             }
         }
     }
