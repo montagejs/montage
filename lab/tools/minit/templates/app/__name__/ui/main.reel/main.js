35ea87941ed0c69429de4b34f94fff277f46a189
Stuart Knightley <stuartk@motorola.com>
2012-07-02 09:09:25 -0700

    Correct require in Minit app template

--- lab/tools/minit/templates/app/__name__/ui/main.reel/main.js
+++ lab/tools/minit/templates/app/__name__/ui/main.reel/main.js
@@ -4,7 +4,7 @@
     @requires montage/ui/component
 */
 var Montage = require("montage").Montage,
-    Component = require("ui/component").Component;
+    Component = require("montage/ui/component").Component;
 
 /**
     Description TODO
