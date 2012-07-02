806b5e832d32a1886934e8bff923d5cf15807e8d
Stuart Knightley <stuartk@motorola.com>
2012-07-02 09:11:37 -0700

    Remove unnecessary property from Minit app template

--- lab/tools/minit/templates/app/__name__/ui/main.reel/main.js
+++ lab/tools/minit/templates/app/__name__/ui/main.reel/main.js
@@ -13,10 +13,6 @@ var Montage = require("montage").Montage,
 */
 exports.Main = Montage.create(Component, /** @lends module:"ui/main.reel".Main# */ {
 
-    hasTemplate: {
-        value: true
-    },
-
     thing: {
         value: "World"
     }
