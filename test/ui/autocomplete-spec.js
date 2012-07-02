d392c6e92103a162f2bbfe680bc7e916938c0fb3
Kishore Subramanian <kishore.subramanian@motorola.com>
2012-07-02 13:33:59 -0700

    Remove waits() from autocomplete-spec. Related to gh-893.

--- test/ui/autocomplete-spec.js
+++ test/ui/autocomplete-spec.js
@@ -41,25 +41,29 @@ var testPage = TestPageLoader.queueTest("autocomplete-test", function() {
                                 expect(test.autocomplete1.value).toBe(text + ",");
                                 expect(test.selectedValue1).toBe(text + ",");
                             });
-                            
+
                         });
                     });
                 });
 
                 describe ("suggestions popup tests", function() {
+                    afterEach(function() {
+                        test.autocomplete1.suggestions = [];
+                    });
+
                     it("can show suggestions", function() {
                         runs(function(){
                             test.autocomplete1.element.value = 'Cal';
                             // simulate the 'input' event on the textfield
                             test.autocomplete1._setValue();
-                            
-                            waits(1000);
+
+                            testPage.waitForDraw(3);
                             runs(function() {
                                 //console.log('test.autocomplete1', test.autocomplete1);
                                 expect(test.autocomplete1.suggestions.count()).toBe(1);
                                 expect(test.autocomplete1.showPopup).toBe(true);
                             });
-                            
+
                         });
                     });
 
@@ -68,92 +72,92 @@ var testPage = TestPageLoader.queueTest("autocomplete-test", function() {
                             test.autocomplete1.element.value = 'ABCD';
                             // simulate the 'input' event on the textfield
                             test.autocomplete1._setValue();
-                            
-                            waits(1000);
+
+                            testPage.waitForDraw(3);
                             runs(function() {
-                                //console.log('test.autocomplete1', test.autocomplete1);
+                                console.log('test.autocomplete1', test.autocomplete1.suggestions);
                                 expect(test.autocomplete1.suggestions.count()).toBe(0);
                                 expect(test.autocomplete1.showPopup).toBe(false);
                             });
-                            
+
                         });
                     });
-                    
+
                     it("can select a suggestion", function() {
                         runs(function(){
                             test.autocomplete1.element.value = 'Cal';
                             // simulate the 'input' event on the textfield
                             test.autocomplete1._setValue();
-                            
-                            waits(1000);
-                            
+
+                            testPage.waitForDraw(3);
+
                             runs(function() {
                                 expect(test.autocomplete1.suggestions.count()).toBe(1);
                                 expect(test.autocomplete1.showPopup).toBe(true);
-                                
+
                                 var event = document.createEvent('CustomEvent');
                                 event.initEvent('keyup', true, true);
                                 event.keyCode = 13;
-                                
+
                                 test.autocomplete1.handleKeyup(event);
                                 testPage.waitForDraw();
                                 //console.log('autocomplete1 suggestedValue', test.autocomplete1.suggestedValue);
-                                
+
                                 runs(function() {
                                     //console.log('autocomplete1 value after accepting suggestion', test.autocomplete1.value);
-                                    expect(test.autocomplete1.value).toBe("California,"); 
-                                    expect(test.selectedValue1).toBe("California,"); 
+                                    expect(test.autocomplete1.value).toBe("California,");
+                                    expect(test.selectedValue1).toBe("California,");
                                 });
-                                                                
+
                             });
-                            
+
                         });
                     });
 
                 });
-                
+
                 it("can select a suggestion and bind the selected value", function() {
                     runs(function(){
                         test.autocomplete1.element.value = 'Cal';
                         // simulate the 'input' event on the textfield
                         test.autocomplete1._setValue();
-                        
-                        waits(1000);
-                        
-                        runs(function() {                            
+
+                        testPage.waitForDraw(3);
+
+                        runs(function() {
                             var event = document.createEvent('CustomEvent');
                             event.initEvent('keyup', true, true);
                             event.keyCode = 13;
-                            
+
                             test.autocomplete1.handleKeyup(event);
                             testPage.waitForDraw();
-                            
+
                             runs(function() {
-                                expect(test.selectedValue1).toBe("California,"); 
+                                expect(test.selectedValue1).toBe("California,");
                             });
-                                                        
+
                         });
-                        
+
                     });
                 });
 
                 describe ("read only testing", function() {
                     it("set the autocomplete read only", function() {
-                        test.autocomplete1.readOnly = true;
-                        waits(150);
+                        test.autocomplete1.readonly = true;
+                        testPage.waitForDraw();
                         runs(function() {
-                            expect(test.autocomplete1.readOnly).toBeTruthy();
+                            expect(test.autocomplete1.readonly).toBeTruthy();
                         });
                     });
-                    
+
                     it("set the autocomplete writable", function() {
-                        test.autocomplete1.readOnly = false;
-                        waits(150);
+                        test.autocomplete1.readonly = false;
+                        testPage.waitForDraw();
                         runs(function() {
-                            expect(test.autocomplete1.readOnly).toBeFalsy();
+                            expect(test.autocomplete1.readonly).toBeFalsy();
                         });
                     });
-                    
+
                 });
             });
         });
