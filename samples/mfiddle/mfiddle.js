47c5441d10cb8deb2a0dddc9a6924fbe85795b05
António Afonso <antonio.afonso@motorola.com>
2012-07-18 19:45:20 -0700

    Format serialization with 4 space indent

--- samples/mfiddle/mfiddle.js
+++ samples/mfiddle/mfiddle.js
@@ -224,7 +224,7 @@ exports.Mfiddle = Montage.create(Component, {
 
     _stringifySerialization: {
         value: function(object) {
-            return JSON.stringify(object, null, 2)
+            return JSON.stringify(object, null, 4)
                 .replace(/\{\s*(\"[#@]\")\s*:\s*(\"[^\"]+\")\s*\}/g, "{$1: $2}")
                 .replace(/\{\s*(\"(?:<-|<->)\")\s*:\s*(\"[^\"]+\"\s*(?:,\s*\"converter\"\s*:\s*\{\s*\"@\"\s*:\s*\"[^\"]+\"\s*\}\s*|,\s*\"deferred\"\s*:\s*(true|false)\s*)*)\}/g, function(_, g1, g2) {
                     return "{" + g1 + ": " + g2.replace(/,\s*/, ", ").replace(/\n\s*/, "") + "}";
