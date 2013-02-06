7bfebcf76dda75acdc86a85a6b43ece3ac74be0a
António Afonso <antonio.afonso@gmail.com>
2013-02-06 14:41:41 -0800

    Fix bindings-spec

--- test/serialization/bindings-spec.js
+++ test/serialization/bindings-spec.js
@@ -1,8 +1,8 @@
 
 var Montage = require("montage").Montage;
 var Bindings = require("montage/core/bindings").Bindings;
-var serialize = require("montage/core/serializer").serialize;
-var deserialize = require("montage/core/deserializer").deserialize;
+var serialize = require("montage/core/serialization").serialize;
+var deserialize = require("montage/core/serialization").deserialize;
 
 var Type = exports.Type = Montage.create(Montage, {
     foo: {
@@ -22,13 +22,15 @@ describe("serialization/bindings-spec", function () {
         var serialization = serialize(object, require);
         var notation = JSON.parse(serialization);
         expect(notation).toEqual({
-            root: {
-                prototype: "serialization/bindings-spec[Type]",
-                properties: {
-                    foo: 10
+            "root": {
+                "prototype": "serialization/bindings-spec[Type]",
+                "properties": {
+                    "foo": 10,
+                    "identifier": null,
+                    "parentProperty": null
                 },
-                bindings: {
-                    bar: {
+                "bindings": {
+                    "bar": {
                         "<-": "foo"
                     }
                 }
@@ -56,36 +58,41 @@ describe("serialization/bindings-spec", function () {
         var serialization = serialize(object, require);
         var notation = JSON.parse(serialization);
         expect(notation).toEqual({
-            root: {
-                prototype: "serialization/bindings-spec[Type]",
-                properties: {
-                    foo: 10
+            "root": {
+                "prototype": "serialization/bindings-spec[Type]",
+                "properties": {
+                    "foo": 10,
+                    "identifier": null,
+                    "parentProperty": null
                 },
-                bindings: {
-                    bar: {
+                "bindings": {
+                    "bar": {
                         "<-": "@montage.foo",
                     }
                 }
             },
 
-            montage: {}
+            "montage": {}
         });
     });
 
     it("should deserialize a simple binding in normal form", function () {
-        return deserialize({
-            root: {
-                prototype: "serialization/bindings-spec[Type]",
-                properties: {
-                    foo: 10
-                },
-                bindings: {
-                    bar: {
-                        "<-": "foo"
+        var serialization = {
+                "root": {
+                    "prototype": "serialization/bindings-spec[Type]",
+                    "properties": {
+                        "foo": 10
+                    },
+                    "bindings": {
+                        "bar": {
+                            "<-": "foo"
+                        }
                     }
                 }
-            }
-        }, require)
+            },
+            serializationString = JSON.stringify(serialization);
+
+        return deserialize(serializationString, require)
         .then(function (object) {
             expect(object.foo).toBe(10);
             object.foo = 20;
@@ -94,49 +101,27 @@ describe("serialization/bindings-spec", function () {
     });
 
     it("should deserialize a simple binding with a component reference", function () {
-        return deserialize({
-            root: {
-                prototype: "serialization/bindings-spec[Type]",
-                properties: {
-                    foo: 10
-                },
-                bindings: {
-                    bar: {
-                        "<-": "@root.foo"
+        var serialization = {
+                "root": {
+                    "prototype": "serialization/bindings-spec[Type]",
+                    "properties": {
+                        "foo": 10
+                    },
+                    "bindings": {
+                        "bar": {
+                            "<-": "@root.foo"
+                        }
                     }
                 }
-            }
-        }, require)
+            },
+            serializationString = JSON.stringify(serialization);
+
+        return deserialize(serializationString, require)
         .then(function (object) {
             expect(object.foo).toBe(10);
             object.foo = 20;
             expect(object.bar).toBe(20);
         });
     });
-
-    // deprecated forms
-    it("should deserialize a simple binding in the old form", function () {
-        return deserialize({
-            root: {
-                prototype: "serialization/bindings-spec[Type]",
-                properties: {
-                    foo: 10
-                },
-                bindings: {
-                    bar: {
-                        boundObject: "root",
-                        boundPropertyPath: "foo",
-                        oneway: true
-                    }
-                }
-            }
-        }, require)
-        .then(function (object) {
-            expect(object.foo).toBe(10);
-            object.foo = 20;
-            expect(object.bar).toBe(20);
-        });
-    });
-
 });
 
