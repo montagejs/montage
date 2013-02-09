dff1494620156b64fdb38ff763e94f8eab657e0b
Kris Kowal <kris.kowal@cixar.com>
2013-02-09 11:22:25 -0800

    Move Montage bindings methods into core/bindings

--- core/core.js
+++ core/core.js
@@ -677,53 +677,13 @@ Object.defineProperty(Montage, "callDelegateMethod", {
 });
 
 var PropertyChanges = require("collections/listen/property-changes");
-var Bindings = require("core/bindings").Bindings;
 
 Object.addEach(Montage, PropertyChanges.prototype);
 
-Montage.defineProperties(Montage, {
-
-    defineBinding: {
-        value: function (targetPath, descriptor, parameters) {
-            return Bindings.defineBinding(this, targetPath, descriptor, parameters);
-        }
-    },
-
-    defineBindings: {
-        value: function (descriptors, parameters) {
-            return Bindings.defineBindings(this, descriptors, parameters);
-        }
-    },
-
-    cancelBinding: {
-        value: function (targetPath) {
-            return Bindings.cancelBinding(this, targetPath);
-        }
-    },
-
-    cancelBindings: {
-        value: function () {
-            return Bindings.cancelBindings(this);
-        }
-    },
-
-    getBinding: {
-        value: function (targetPath) {
-            return Bindings.getBinding(this, targetPath);
-        }
-    },
-
-    getBindings: {
-        value: function () {
-            return Bindings.getBindings(this);
-        }
-    }
-
-});
-
+// have to come last since they use the Montage.defineProperties to augment Object.prototype
+require("core/bindings");
+require("core/paths");
 // has to come last since serializer and deserializer depend on logger, which
 // in turn depends on montage running to completion
 require("core/serialization-bindings");
-// has to come last since it uses the Montage.defineProperties to augment Object.prototype
-require("core/paths");
 
