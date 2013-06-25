74252d543ce5fb4a21c37526b7a237c39c3c22ed
cesine <cesine@yahoo.com>
2013-06-24 22:10:42 -0400

    added a test for clicking twice on a selected element

--- test/ui/repetition-selection-spec.js
+++ test/ui/repetition-selection-spec.js
@@ -42,6 +42,21 @@ TestPageLoader.queueTest("repetition/selection-test/selection-test", function(te
                 });
             });
 
+            it("should not deselect the representative element if clicked again", function () {
+                var listElementToSelect = querySelectorAll("ul>li")[1];
+                testPage.mouseEvent({target: listElementToSelect}, "mousedown", function () {
+                    testPage.mouseEvent({target: listElementToSelect}, "mouseup", function () {
+                        expect(listElementToSelect.classList.contains("selected")).toBeTruthy();
+
+                        testPage.mouseEvent({target: listElementToSelect}, "mousedown", function () {
+                            testPage.mouseEvent({target: listElementToSelect}, "mouseup", function () {
+                                expect(listElementToSelect.classList.contains("selected")).toBeTruthy();
+                            });
+                        });
+                    });
+                });
+            });
+
         });
 
         describe("reflecting the contentController selection", function () {
