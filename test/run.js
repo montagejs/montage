f8d3d1409b9a148944b2e0c630b2981437f866bb
António Afonso <antonio.afonso@gmail.com>
2013-07-11 01:10:35 -0700

    Add ModalOverlay

--- test/run.js
+++ test/run.js
@@ -85,6 +85,7 @@ require("montage-testing").run(require,[
         "ui/condition-spec",
         "ui/firstdraw-spec",
         "ui/loader-spec",
+        "ui/modal-overlay-spec",
         "ui/overlay-spec",
         // Broken due to changes to repetition
         "ui/repetition-spec",
