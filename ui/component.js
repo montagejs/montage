c61e0ed6bd7da393a4c438cc71bcc8c8c6311ca5
Hugo Gomes <mr.hugo.gomes@gmail.com>
2013-08-12 11:30:32 +0100

    Renamed requestAnimationFrame/cancelAnimationFrame to use unprefixed versions first

--- ui/component.js
+++ ui/component.js
@@ -2774,7 +2774,8 @@ var RootComponent = Component.specialize( /** @lends RootComponent# */{
      * @function
      */
     requestAnimationFrame: {
-        value: (window.webkitRequestAnimationFrame ? window.webkitRequestAnimationFrame : window.mozRequestAnimationFrame),
+        value: (window.requestAnimationFrame || window.webkitRequestAnimationFrame 
+             || window.mozRequestAnimationFrame ||  window.msRequestAnimationFrame),
         enumerable: false
     },
 
@@ -2783,7 +2784,8 @@ var RootComponent = Component.specialize( /** @lends RootComponent# */{
      * @function
      */
     cancelAnimationFrame: {
-        value: (window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame : window.mozCancelRequestAnimationFrame),
+        value: (window.cancelAnimationFrame ||  window.webkitCancelAnimationFrame 
+             || window.mozCancelAnimationFrame || window.msCancelAnimationFrame),
         enumerable: false
     },
 
