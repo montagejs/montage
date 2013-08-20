5bfea638e2b35e6bea2d2c4f1f40c27e55b195e7
Adam Solove <asolove@gmail.com>
2013-08-19 18:21:23 -0700

    CurrencyConverter now respects the shorten and round properties from NumberConverter and has a showCurrencyBeforeNumber option for e.g. '$ 12'

--- core/converter/currency-converter.js
+++ core/converter/currency-converter.js
@@ -10,26 +10,7 @@ var Montage = require("montage").Montage;
 var Converter = require('core/converter/converter');
 var numericValueToString = require("core/converter/number-converter").numericValueToString;
 var NumberConverter = require("core/converter/number-converter").NumberConverter;
-/**
- * Formats a number as a human-readable currency value.
- * @private
- * @function #formatCurrency
- * @param {Property} value
- * @param {String} currency
- * @param {Number} decimals
- * @param {String} useParensForNegative
- * @returns stringValue
- */
-var formatCurrency = function(value, currency, decimals, useParensForNegative) {
-    var stringValue = numericValueToString(value, decimals);
-    currency = currency || '$';
-    if ((value < 0) && useParensForNegative) {
-        stringValue = '(' + stringValue.substring(1, stringValue.length) + ')';
-    }
 
-    stringValue = stringValue + ' ' + currency;
-    return stringValue;
-};
 /**
  * @class CurrencyConverter
  * @classdesc Formats a value as a currency.
@@ -62,13 +43,31 @@ exports.CurrencyConverter = NumberConverter.specialize( /** @lends CurrencyConve
     },
 
     /**
+     * @type {Boolean}
+     * @default {Boolean} false
+     */
+    showCurrencyBeforeNumber: {
+        value: false
+    },
+
+    /**
      * @function
      * @param {Number} amount
      * @returns {String} The formatted currency value.
      */
     convert: {
         value: function(amount) {
-            return formatCurrency(amount, this.currency, this.decimals, this.useParensForNegative);
+            var stringValue = this.super(amount);
+            if ((amount < 0) && useParensForNegative) {
+                stringValue = '(' + stringValue.substring(1, stringValue.length) + ')';
+            }
+
+            if(this.showCurrencyBeforeNumber) {
+                stringValue = this.currency + ' ' + stringValue;
+            } else {
+                stringValue = stringValue + ' ' + this.currency;
+            }
+            return stringValue;
         }
     }
 
