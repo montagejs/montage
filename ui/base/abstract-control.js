9b2901d0c13403453ecd08e880f9c183d8ffc316
cesine <cesine@yahoo.com>
2013-09-17 22:52:33 -0400

    adding standard input attributes

--- ui/base/abstract-control.js
+++ ui/base/abstract-control.js
@@ -7,7 +7,7 @@ var Montage = require("montage").Montage,
  * @class AbstractControl
  * @extends Component
  */
-exports.AbstractControl = Component.specialize( /** @lends AbstractControl# */ {
+var AbstractControl = exports.AbstractControl = Component.specialize( /** @lends AbstractControl# */ {
 
     dispatchActionEvent: {
         value: function() {
@@ -44,4 +44,36 @@ exports.AbstractControl = Component.specialize( /** @lends AbstractControl# */ {
             return actionEvent;
         }
     }
+
+});
+
+// Standard <input> tag attributes - http://www.w3.org/TR/html5/the-input-element.html#the-input-element
+AbstractControl.addAttributes({
+    accept: null,
+    alt: null,
+    autocomplete: null,
+    autofocus: {dataType: "boolean"},
+    contenteditable: {dataType: "boolean"},
+    disabled: {dataType: 'boolean'},
+    form: null,
+    formaction: null,
+    formenctype: null,
+    formmethod: null,
+    formnovalidate: {dataType: 'boolean'},
+    formtarget: null,
+    height: null,
+    list: null,
+    maxlength: null,
+    multiple: {dataType: 'boolean'},
+    name: null,
+    pattern: null,
+    placeholder: null,
+    readonly: {dataType: 'boolean'},
+    required: {dataType: 'boolean'},
+    size: null,
+    src: null,
+    style: null,
+    title: null,
+    width: null
+    // "type" is not bindable and "value" is handled as a special attribute
 });
