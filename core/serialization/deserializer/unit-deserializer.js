c8dc5676a15f7a5026f5864a094e49efc9c90b8d
António Afonso <antonio.afonso@gmail.com>
2013-10-21 16:14:55 -0700

    Add template property bindings in deserialization
    Ignore bindings to template properties that can not be resolved yet, this happens with repetition's content.
    
    Fail on template properties that refer components that do not exist in the serialization.
    
    Since labels can now have a colon we can use this to speed up template property's look ups during bindings by creating an entry in the serialization that matches both component and property name like "repetition:iteration": {}. It's less flexible because the exact right label needs to be computed but faster during template creation and instantiation.
    
    Also improved error detection on bindings to components that do not exist in the serialization.

--- core/serialization/deserializer/unit-deserializer.js
+++ core/serialization/deserializer/unit-deserializer.js
@@ -18,12 +18,46 @@ var UnitDeserializer = Montage.specialize( {
         }
     },
 
+    _templatePropertyRegExp: {
+        value: /^([^:]+)(:.*)$/
+    },
+
+    /**
+     * A valid template property reference is one that references a component
+     * that exists and has the format: @<component>:<property>[.path].
+     */
+    isValidTemplatePropertyReference: {
+        value: function(label) {
+            var templateProperty = this._templatePropertyRegExp.exec(label);
+
+            if (templateProperty) {
+                var componentLabel = templateProperty[1];
+
+                if (this._context.hasObject(componentLabel)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    },
+
     getObjectByLabel: {
         value: function(label) {
-            var object = this._context.getObject(label);
-
-            if (!Promise.isPromise(object)) {
-                return object;
+            if (this._context.hasObject(label)) {
+                // All labels that exist are immediately resolved into an object
+                // even if they are a valid template property reference. This
+                // "trick" can be used to speed up template property's look ups.
+                return this._context.getObject(label);
+            } else if (this.isValidTemplatePropertyReference(label)) {
+                // Ignore valid template property references that are not
+                // available yet. This can happen during the instantiation of
+                // the repetition's content.
+                return null;
+            } else {
+                throw new Error("Object with label '" + label + "' was not found.");
             }
         }
     }
--- test/serialization/bindings-spec.js
+++ test/serialization/bindings-spec.js
@@ -3,6 +3,7 @@ var Montage = require("montage").Montage;
 var Bindings = require("montage/core/bindings").Bindings;
 var serialize = require("montage/core/serialization").serialize;
 var deserialize = require("montage/core/serialization").deserialize;
+var Deserializer = require("montage/core/serialization").Deserializer;
 
 var Type = exports.Type = Montage.specialize( {
     foo: {
@@ -120,6 +121,110 @@ describe("serialization/bindings-spec", function () {
             expect(object.foo).toBe(10);
             object.foo = 20;
             expect(object.bar).toBe(20);
+        });
+    });
+
+    it("should fail deserializing a binding to a non existing object", function () {
+        var serialization = {
+                "root": {
+                    "prototype": "serialization/bindings-spec[Type]",
+                    "properties": {
+                        "foo": 10
+                    },
+                    "bindings": {
+                        "bar": {"<-": "@unknown.foo"}
+                    }
+                }
+            },
+            serializationString = JSON.stringify(serialization);
+
+        return deserialize(serializationString, require)
+        .then(function (object) {
+            expect("deserialization").toBe("fail");
+        }).fail(function() {
+            // it should fail
+        });
+    });
+
+    describe("template properties' bindings", function() {
+        var deserializer;
+
+        beforeEach(function() {
+            deserializer = new Deserializer();
+        });
+
+        it("should not allow binding to a template property of a component that does not exist", function() {
+            var serialization = {
+                    "component": {
+                        "prototype": "montage/ui/component",
+                        "bindings": {
+                            "value": {"<-": "@unknown:templateProperty"}
+                        }
+                    }
+                },
+                serializationString = JSON.stringify(serialization);
+
+            deserializer.init(serializationString, require);
+
+            return deserializer.deserialize()
+            .then(function() {
+                expect("deserialization").toBe("failed");
+            }).fail(function() {
+                // it should fail
+            });
+        });
+
+        it("should allow binding to a template property of a component that exists", function() {
+            var serialization = {
+                    "known": {},
+
+                    "component": {
+                        "prototype": "montage/ui/component",
+                        "bindings": {
+                            "value": {"<-": "@known:templateProperty"}
+                        }
+                    }
+                },
+                instances = {
+                    "known": {}
+                },
+                serializationString = JSON.stringify(serialization);
+
+            deserializer.init(serializationString, require);
+
+            return deserializer.deserialize(instances)
+            .then(function() {
+                // this is here just to consume the promise result
+            });
+        });
+
+        it("should bind correctly to a component with a colon", function() {
+            // This "trick" can be used to speed up template properties'
+            // resolution at bind time.
+            var serialization = {
+                    "owner": {},
+                    "owner:templateProperty": {},
+
+                    "component": {
+                        "prototype": "montage/ui/component",
+                        "bindings": {
+                            "value": {"<-": "@owner:templateProperty"}
+                        }
+                    }
+                },
+                owner = {},
+                instances = {
+                    "owner": owner,
+                    "owner:templateProperty": {}
+                },
+                serializationString = JSON.stringify(serialization);
+
+            deserializer.init(serializationString, require);
+
+            return deserializer.deserialize(instances)
+            .then(function(objects) {
+                expect(objects.component.value).toBe(instances["owner:templateProperty"]);
+            });
         });
     });
 });
