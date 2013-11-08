eff4bd101a5ea10666d4989ce1fcc98fbed75d2f
Nikita Vasilyev <me@elv1s.ru>
2013-11-08 00:53:05 -0500

    Add a simplest example of repetition switchPath.

---
+++ examples/repetition/main.reel/main.js
@@ -0,0 +1,7 @@
+var Montage = require("montage").Montage,
+    Component = require("ui/component").Component,
+    RangeController = require("core/range-controller").RangeController;
+
+exports.Main = Montage.create(Component, {
+
+});
