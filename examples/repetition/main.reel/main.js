16fb5d5af1b757fddc74560c19bfc1346c21a033
Nikita Vasilyev <me@elv1s.ru>
2013-11-21 05:05:43 -0500

    Remove example.

--- examples/repetition/main.reel/main.js
+++
@@ -1,7 +0,0 @@
-var Montage = require("montage").Montage,
-    Component = require("ui/component").Component,
-    RangeController = require("core/range-controller").RangeController;
-
-exports.Main = Montage.create(Component, {
-
-});
