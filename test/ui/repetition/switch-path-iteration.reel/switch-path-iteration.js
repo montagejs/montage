43d66ea42541f3edad7948c060e04bca95475e28
Nikita Vasilyev <me@elv1s.ru>
2013-11-26 13:00:18 -0500

    Failing attempt to implement more comprehensive test.

--- test/ui/repetition/switch-path-iteration.reel/switch-path-iteration.js
+++ test/ui/repetition/switch-path-iteration.reel/switch-path-iteration.js
@@ -3,5 +3,13 @@ var Montage = require("montage").Montage,
     RangeController = require("montage/core/range-controller").RangeController;
 
 exports.SwitchPathIteration = Montage.create(Component, {
-
+    _isEven: {
+        value: true
+    },
+    isEven: {
+        get: function() {
+            this._isEven = !this._isEven;
+            return this._isEven;
+        }
+    }
 });
