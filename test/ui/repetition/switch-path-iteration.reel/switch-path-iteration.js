aa0ad762cfff3b5447a48bda0a9f09630f8b0af4
Nikita Vasilyev <me@elv1s.ru>
2013-11-26 12:02:56 -0500

    Add second test.

---
+++ test/ui/repetition/switch-path-iteration.reel/switch-path-iteration.js
@@ -0,0 +1,7 @@
+var Montage = require("montage").Montage,
+    Component = require("montage/ui/component").Component,
+    RangeController = require("montage/core/range-controller").RangeController;
+
+exports.SwitchPathIteration = Montage.create(Component, {
+
+});
