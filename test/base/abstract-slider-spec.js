34c7a9108ed3766d76137c746355a4df6426e08f
François Frisch <francoisfrisch@gmail.com>
2013-11-27 12:22:19 -0800

    Fix errors in abstract-slider-spec
    The handleMousedown was missing the event argument. Added a mock event
    and added it for all the others too.

--- test/base/abstract-slider-spec.js
+++ test/base/abstract-slider-spec.js
@@ -2,6 +2,7 @@
 var Montage = require("montage").Montage;
 var AbstractSlider = require("montage/ui/base/abstract-slider").AbstractSlider;
 var MockDOM = require("mocks/dom");
+var MockEvent = require("mocks/event");
 
 AbstractSlider.prototype.hasTemplate = false;
 
@@ -78,19 +79,19 @@ describe("test/base/abstract-slider-spec", function () {
                 expect(aSlider.active).toBeFalsy();
             });
             it("should be true after touchstart", function() {
-                aSlider.handleTouchstart();
+                aSlider.handleTouchstart(MockEvent.event("touchestart"));
                 expect(aSlider.active).toBeTruthy();
             });
             it("should be true after mousedown", function() {
-                aSlider.handleMousedown();
+                aSlider.handleMousedown(MockEvent.event("mousedown"));
                 expect(aSlider.active).toBeTruthy();
             });
             it("should be false after mouseup", function() {
-                aSlider.handleMouseup();
+                aSlider.handleMouseup(MockEvent.event("mouseup"));
                 expect(aSlider.active).toBeFalsy();
             });
             it("should be false after touchend", function() {
-                aSlider.handleTouchend();
+                aSlider.handleTouchend(MockEvent.event("touchend"));
                 expect(aSlider.active).toBeFalsy();
             });
             it("should be false after thumbTranslateEnd", function() {
