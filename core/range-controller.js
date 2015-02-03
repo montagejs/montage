8f6e6798cc6efa2444ccf042d26874ccfc34d14b
Benoit Marchant <marchant@mac.com>
2015-02-02 16:11:19 -0800

    Fixes regressions in tests

--- core/range-controller.js
+++ core/range-controller.js
@@ -96,7 +96,7 @@ var RangeSelection = function(content, rangeController) {
 
 			
             var minus;
-            if (this.length === 0) {
+            if (minusLength === 0) {
                 // minus will be empty
                 if (plus.length === 0) {
                     // at this point if plus is empty there is nothing to do.
@@ -104,7 +104,7 @@ var RangeSelection = function(content, rangeController) {
                 }
                 minus = EMPTY_ARRAY;
             } else {
-                minus = Array.prototype.slice.call(this, start, start + length);
+                minus = Array.prototype.slice.call(this, start, start + minusLength);
             }
             var diff = plus.length - minus.length;
             var newLength = Math.max(this.length + diff, start + plus.length);
