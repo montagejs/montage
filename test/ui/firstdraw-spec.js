8c53ba5fda28b59d3fe3899e269f284e3de274e5
Ray Shan <ray@shan.io>
2015-02-25 18:45:57 -0800

    fix first draw tests

--- test/ui/firstdraw-spec.js
+++ test/ui/firstdraw-spec.js
@@ -1,37 +1,42 @@
 /* <copyright>
-Copyright (c) 2012, Motorola Mobility LLC.
-All Rights Reserved.
+ Copyright (c) 2012, Motorola Mobility LLC.
+ All Rights Reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
 
-* Redistributions of source code must retain the above copyright notice,
-  this list of conditions and the following disclaimer.
+ * Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
 
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
 
-* Neither the name of Motorola Mobility LLC nor the names of its
-  contributors may be used to endorse or promote products derived from this
-  software without specific prior written permission.
+ * Neither the name of Motorola Mobility LLC nor the names of its
+ contributors may be used to endorse or promote products derived from this
+ software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-</copyright> */
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ </copyright>
+ */
+
 var Montage = require("montage").Montage,
     TestPageLoader = require("montage-testing/testpageloader").TestPageLoader;
 
-TestPageLoader.queueTest("firstdraw-simple", {src: "ui/drawing/simple.html", firstDraw: true}, function (simpleTestPage) {
+TestPageLoader.queueTest("firstdraw-simple", {
+    src: "ui/drawing/simple.html",
+    firstDraw: true
+}, function (simpleTestPage) {
     describe("ui/firstdraw-spec", function () {
         describe("component", function () {
             it("should draw within first draw", function () {
@@ -55,18 +60,16 @@ TestPageLoader.queueTest("firstdraw-simple", {src: "ui/drawing/simple.html", fir
     });
 });
 
-TestPageLoader.queueTest("firstdraw-repetition", {src: "ui/drawing/repetition.html", firstDraw: true}, function (repetitionTestPage) {
+TestPageLoader.queueTest("firstdraw-repetition", {
+    src: "ui/drawing/repetition.html",
+    firstDraw: true
+}, function (repetitionTestPage) {
     describe("Drawing Repetition", function () {
         describe("repeating component", function () {
-            it("TODO should draw within first draw", function () {
-
-                repetitionTestPage.waitForComponentDraw(repetitionTestPage.test.repetition1);
-
-                runs(function () {
-                    var text0 = repetitionTestPage.iframe.contentDocument.querySelectorAll(".list1 > div")[0];
-                    expect(text0).not.toBeFalsy();
-                    expect(text0.textContent).toEqual("Test Value");
-                });
+            it("should draw within first draw", function () {
+                var text0 = repetitionTestPage.iframe.contentDocument.querySelectorAll(".list1 > div")[0];
+                expect(text0).not.toBeFalsy();
+                expect(text0.textContent).toEqual("Test Value");
             });
         });
         describe("repeating component with template", function () {
@@ -78,73 +81,81 @@ TestPageLoader.queueTest("firstdraw-repetition", {src: "ui/drawing/repetition.ht
         });
     });
 });
-//var cancelDrawTestPage = TestPageLoader.queueTest("firstdraw-cancel-draw", {src: "ui/drawing/cancel-draw.html", firstDraw: false}, function () {
-//    describe("Canceling a component draw", function () {
-//        it("should load", function () {
-//            expect(cancelDrawTestPage.loaded).toBeTruthy();
-//        });
-//        it("should not call draw on a template based component where needsDraw has been set to false", function () {
-//            // setup spies
-//            spyOn(cancelDrawTestPage.test.sliderC, 'draw').andCallThrough();
-//            spyOn(cancelDrawTestPage.test.customC, 'draw').andCallThrough();
-//
-//            // Setup drawing
-//            cancelDrawTestPage.test.slotC.content = cancelDrawTestPage.test.sliderC;
-//            cancelDrawTestPage.test.sliderC.needsDraw = false;
-//            cancelDrawTestPage.test.slotC.content = cancelDrawTestPage.test.customC;
-//
-//            cancelDrawTestPage.waitForDraw(2);
-//
-//            runs(function () {
-//                expect(cancelDrawTestPage.test.sliderC.needsDraw).toBeFalsy();
-//                expect(cancelDrawTestPage.test.customC.needsDraw).toBeFalsy();
-//                expect(cancelDrawTestPage.test.sliderC.draw).not.toHaveBeenCalled();
-//                expect(cancelDrawTestPage.test.customC.draw).toHaveBeenCalled();
-//            });
-//        });
-//    });
-//});
-//var templateParametersTestPage = TestPageLoader.queueTest("firstdraw-template-parameters", {src: "ui/drawing/template-parameters.html", firstDraw: true}, function () {
-//    describe("Template parameters draw", function () {
-//        it("should load", function () {
-//            expect(templateParametersTestPage.loaded).toBeTruthy();
-//        });
-//
-//        it("should replace the template parameters with the component dom arguments", function () {
-//            var decorator = templateParametersTestPage.test.decorator,
-//                element = decorator.element;
-//
-//            expect(element.children.length).toBe(1);
-//            expect(element.children[0].children.length).toBe(2);
-//        });
-//
-//        it("should pass the component arguments to another component arguments using template parameters", function () {
-//            var compositionDecorator = templateParametersTestPage.test.compositionDecorator,
-//                element = compositionDecorator.element,
-//                decoration;
-//
-//            decoration = element.querySelector(".decoration");
-//            expect(decoration.children.length).toBe(2);
-//        });
-//
-//        it("should pass the component arguments to another component arguments along with other elements using template parameters", function () {
-//            var compositionDecoratorMore = templateParametersTestPage.test.compositionDecoratorMore,
-//                element = compositionDecoratorMore.element,
-//                decoration;
-//
-//            decoration = element.querySelector(".decoration");
-//            expect(decoration.children.length).toBe(2);
-//        });
-//
-//        it("should replace the template named parameters with the component dom arguments", function () {
-//            var parameters = templateParametersTestPage.test.parameters,
-//                element = parameters.element,
-//                item1 = element.querySelector(".item1 > span"),
-//                item2 = element.querySelector(".item2 > span");
-//
-//            expect(item1.textContent).toBe("One");
-//            expect(item2.textContent).toBe("Two");
-//        });
-//    });
-//});
-//
+
+TestPageLoader.queueTest("firstdraw-cancel-draw", {
+    src: "ui/drawing/cancel-draw.html",
+    firstDraw: false
+}, function (testPage) {
+    describe("Canceling a component draw", function () {
+        it("should load", function () {
+            expect(testPage.loaded).toBeTruthy();
+        });
+        it("should not call draw on a template based component where needsDraw has been set to false", function () {
+            // setup spies
+            spyOn(testPage.test.sliderC, 'draw').andCallThrough();
+            spyOn(testPage.test.customC, 'draw').andCallThrough();
+
+            // Setup drawing
+            testPage.test.slotC.content = testPage.test.sliderC;
+            testPage.test.sliderC.needsDraw = false;
+            testPage.test.slotC.content = testPage.test.customC;
+
+            testPage.waitForDraw(2);
+
+            runs(function () {
+                expect(testPage.test.sliderC.needsDraw).toBeFalsy();
+                expect(testPage.test.customC.needsDraw).toBeFalsy();
+                expect(testPage.test.sliderC.draw).not.toHaveBeenCalled();
+                expect(testPage.test.customC.draw).toHaveBeenCalled();
+            });
+        });
+    });
+});
+
+TestPageLoader.queueTest("firstdraw-template-parameters", {
+    src: "ui/drawing/template-parameters.html",
+    firstDraw: true
+}, function (testPage) {
+    describe("Template parameters draw", function () {
+        it("should load", function () {
+            expect(testPage.loaded).toBeTruthy();
+        });
+
+        it("should replace the template parameters with the component dom arguments", function () {
+            var decorator = testPage.test.decorator,
+                element = decorator.element;
+
+            expect(element.children.length).toBe(1);
+            expect(element.children[0].children.length).toBe(2);
+        });
+
+        it("should pass the component arguments to another component arguments using template parameters", function () {
+            var compositionDecorator = testPage.test.compositionDecorator,
+                element = compositionDecorator.element,
+                decoration;
+
+            decoration = element.querySelector(".decoration");
+            expect(decoration.children.length).toBe(2);
+        });
+
+        it("should pass the component arguments to another component arguments along with other elements using template parameters", function () {
+            var compositionDecoratorMore = testPage.test.compositionDecoratorMore,
+                element = compositionDecoratorMore.element,
+                decoration;
+
+            decoration = element.querySelector(".decoration");
+            expect(decoration.children.length).toBe(2);
+        });
+
+        it("should replace the template named parameters with the component dom arguments", function () {
+            var parameters = testPage.test.parameters,
+                element = parameters.element,
+                item1 = element.querySelector(".item1 > span"),
+                item2 = element.querySelector(".item2 > span");
+
+            expect(item1.textContent).toBe("One");
+            expect(item2.textContent).toBe("Two");
+        });
+    });
+});
+
