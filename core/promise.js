3fb4e521c238c2d99f66dba76dc5a517fd98e560
Benoit Marchant <marchant@mac.com>
2015-05-28 15:44:23 -0700

    Adding low hanging fruit type compatibility/aliasing transition support from q to bluebird,  borrowing from https://github.com/petkaantonov/bluebird-q/

--- core/promise.js
+++ core/promise.js
@@ -5,3 +5,94 @@
 //exports.Promise = require("q");
 exports.Promise = require("bluebird");
 
+var THIS = {};
+var INSTANCE = {};
+
+var aliasMap = {
+    thenReturn: "thenResolve",
+    thenThrow: "thenReject",
+    caught: "fail",
+    progressed: "progress",
+    lastly: "fin",
+    call: "send mcall invoke".split(" ")
+};
+
+var staticAliasMap = {
+    reject: "reject rejected".split(" "),
+    resolve: "resolve resolved fulfill fulfilled".split(" "),
+    is: "isPromise",
+    race: "race",
+    async: "coroutine",
+    spawn: "spawn",
+    delay: "delay",
+
+    timeout: INSTANCE,
+    join: INSTANCE,
+    spread: INSTANCE,
+    tap: INSTANCE,
+    thenReject: INSTANCE,
+    thenResolve: INSTANCE,
+    isPending: INSTANCE,
+    isFulfilled: INSTANCE,
+    isRejected: INSTANCE,
+    get: INSTANCE,
+    set: INSTANCE,
+    del: INSTANCE,
+    'delete': INSTANCE,
+    'try': INSTANCE,
+    keys: INSTANCE,
+    allResolved: INSTANCE,
+    allSettled: INSTANCE,
+    fail: INSTANCE,
+    "catch": INSTANCE,
+    progress: INSTANCE,
+    fin: INSTANCE,
+    "finally": INSTANCE,
+    done: INSTANCE,
+    timeout: INSTANCE,
+    nodeify: INSTANCE,
+    nfapply: INSTANCE,
+    nfcall: INSTANCE,
+    nfbind: INSTANCE,
+    npost: INSTANCE,
+    nsend: INSTANCE,
+    ninvoke: INSTANCE,
+    nbind: INSTANCE,
+    nmcall: INSTANCE,
+    fapply: INSTANCE,
+    fcall: INSTANCE,
+    fbind: INSTANCE,
+    denodeify: INSTANCE,
+    nmapply: INSTANCE,
+    mapply: INSTANCE,
+    post: INSTANCE,
+    send: INSTANCE,
+    mcall: INSTANCE,
+    invoke: INSTANCE
+};
+
+Object.keys(aliasMap).forEach(function(key) {
+    var Qmethods = aliasMap[key];
+    if (!Array.isArray(Qmethods)) Qmethods = [Qmethods];
+    Qmethods.forEach(function(Qmethod) {
+        Promise.prototype[Qmethod] = Promise.prototype[key];
+    });
+});
+
+Object.keys(staticAliasMap).forEach(function(key) {
+    var Qmethods = staticAliasMap[key];
+    if (Qmethods === INSTANCE) {
+        if (typeof Promise.prototype[key] !== "function")
+            throw new Error("unimplemented Promise.prototype." + key);
+        Q[key] = function(promise) {
+            var instance = Q(promise);
+            return instance[key].apply(instance, [].slice.call(arguments, 1));
+        };
+    } else {
+        if (!Array.isArray(Qmethods)) Qmethods = [Qmethods];
+        Qmethods.forEach(function(Qmethod) {
+            Q[Qmethod] = Promise[key];
+        });
+    }
+});
+
