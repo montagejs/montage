92fbf5c784315d634e03cc6237569be0c272cb80
Thibault Zanini <thibaultzanini@gmail.com>
2015-10-23 10:12:28 -0700

    Makes sure a component detaches all its child components when its domContent change.
    The property childComponents is an array, so the length is changing when an element is removed of it. Therefore, all child components were not detached.

--- ui/component.js
+++ ui/component.js
@@ -1067,7 +1067,8 @@ var Component = exports.Component = Target.specialize( /** @lends Component.prot
 
             // cleanup current content
             components = this.childComponents;
-            for (i = 0, component; (component = components[i]); i++) {
+
+            while ((component = components[0])) {
                 component.detachFromParentComponent();
             }
 
