c723f6ce062eb1558ba5e85ed31a013f6fd13c12
Thibault Zanini <thibaultzanini@gmail.com>
2016-02-16 19:53:53 -0800

    Can set values to enumerations and list members.

--- core/enum.js
+++ core/enum.js
@@ -16,15 +16,23 @@ exports.Enum = Montage.specialize( /** @lends Enum# */ {
         value: 0
     },
 
+    _members: {
+        value: null
+    },
+
+    members: {
+        get: function () {
+            return this._members || (this._members = Object.keys(this));
+        }
+    },
+
     /**
      * @function
      * @returns itself
      */
     init : {
         value: function () {
-            if (Object.isSealed(this)) {
-                logger.error(this, "Object is sealed");
-            }
+            this._logErrorIfAlreadySeal();
             return this;
         }
     },
@@ -35,37 +43,57 @@ exports.Enum = Montage.specialize( /** @lends Enum# */ {
      */
     initWithMembers : {
         value: function () {
-            if (Object.isSealed(this)) {
-                logger.error(this, "Object is sealed");
+            if (this._logErrorIfAlreadySeal()) {
                 return this;
             }
-            var member, i;
-            for (i = 0; typeof (member = arguments[i]) !== "undefined"; i++) {
-                if (member !== null) {
-                    this.addMember(member);
-                } else {
-                    logger.error(this, "Member at index " + i + " is null");
-                }
-            }
+
+            this._addMembers(arguments);
+
             return Object.seal(this);
         }
     },
 
+
+    /**
+     * @function
+     * @param {Array.<string>} array of keys.
+     * @param {Array} array of values.
+     * @returns {Object} Object.seal(this)
+     */
+    initWithMembersAndValues : {
+        value: function (members, values) {
+            if (Array.isArray(members) && Array.isArray(values)) {
+                if (members.length === values.length) {
+                    if (this._logErrorIfAlreadySeal()) {
+                        return this;
+                    }
+
+                    this._addMembers(members, values);
+
+                } else {
+                    throw new Error("the number of members must equal to the number of values");
+                }
+            }
+
+            return Object.seal(this);
+        }
+    },
+
+
     /**
      * @function
      * @param {string} member The member to be added.
+     * @param value
      */
     addMember : {
-        value: function (member) {
-
+        value: function (member, value) {
             if (typeof this[member] === "undefined") {
                 Object.defineProperty(this, member, {
                     writable: false,
                     configurable: false,
                     enumerable: true,
-                    value: this._value
+                    value: value !== void 0 && value !== null ? value : this._value++
                 });
-                this._value++;
             }
         }
     },
@@ -76,11 +104,46 @@ exports.Enum = Montage.specialize( /** @lends Enum# */ {
      */
     seal : {
         value: function () {
-            if (! Object.isSealed(this)) {
+            if (!Object.isSealed(this)) {
                 return Object.seal(this);
             }
         }
-    }
+    },
+
+
+    _logErrorIfAlreadySeal: {
+        value: function () {
+            if (Object.isSealed(this)) {
+                logger.error(this, "Object is sealed");
+
+                return true;
+            }
+
+            return false;
+        }
+    },
+
+    _addMembers: {
+        value: function (members, values) {
+            var member, i, value;
+
+            for (i = 0; typeof (member = members[i]) !== "undefined"; i++) {
+                if (member !== null) {
+                    if (values) {
+                        value = values[i];
+
+                        if (value === void 0 && value === null) {
+                            throw new Error("A value of an enumeration cannot be null or undefined")
+                        }
+                    }
+
+                    this.addMember(member, value);
+                } else {
+                    logger.error(this, "Member at index " + i + " is null");
+                }
+            }
+        }
+    },
 
 });
 
