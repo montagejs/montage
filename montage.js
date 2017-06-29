063d66c050778179fe304f40ffaf553acbe24a53
Harold Thetiot <hthetiot@gmail.com>
2017-06-28 19:49:11 -0700

    fix montage amd loader

--- montage.js
+++ montage.js
@@ -2,9 +2,7 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['exports', 'require', 'bluebird'], function (exports, require, bluebird) {
-            factory((root.Montage = exports), require, bluebird);
-        });
+        define('montage', ['exports', 'require', 'bluebird'], factory);
     } else if (typeof exports === 'object') {
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
