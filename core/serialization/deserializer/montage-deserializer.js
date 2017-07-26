1be69e7ac28a6d89a964f4d052b4a7032f84424a
Harold Thetiot <hthetiot@gmail.com>
2017-07-25 21:20:55 -0700

    restore core/jshint instead of npm node module

--- core/serialization/deserializer/montage-deserializer.js
+++ core/serialization/deserializer/montage-deserializer.js
@@ -101,7 +101,7 @@ var MontageDeserializer = exports.MontageDeserializer = Montage.specialize({
                 gutterSize,
                 line;
 
-            return require.async("jshint/dist/jshint").then(function (module) {
+            return require.async("core/jshint").then(function (module) {
                 if (!module.JSHINT(source)) {
                     error = module.JSHINT.errors[0];
                     lines = source.split("\n");
--- montage.js
+++ montage.js
@@ -517,7 +517,7 @@
                     
                     // install the linter, which loads on the first error
                     config.lint = function (module) {
-                        montageRequire.async("jshint/dist/jshint")
+                        montageRequire.async("core/jshint")
                         .then(function (JSHINT) {
                             if (!JSHINT.JSHINT(module.text)) {
                                 console.warn("JSHint Error: "+module.location);
