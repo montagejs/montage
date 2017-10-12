a39789cebb33a72a92837f3898e7f733d10a64e4
Thibault Zanini <thibaultzanini@gmail.com>
2017-10-12 17:45:55 -0400

    Add tree-list.info

---
+++ ui/tree-list.info/sample/ui/main.reel/main.js
@@ -0,0 +1,129 @@
+var Component = require("montage/ui/component").Component,
+    Promise = require('montage/core/promise');
+
+exports.Main = Component.specialize(/** @lends Main# */{
+
+    constructor: {
+        value: function () {
+            this.root = {
+                children: [
+                    {
+                        filename: 'a',
+                        isDirectory: true,
+                        children: [
+                            {
+                                filename: 'a.1',
+                                isDirectory: true,
+                                children: [
+                                    {
+                                        filename: 'a.1.1',
+                                        isDirectory: false
+                                    },
+                                    {
+                                        filename: 'a.1.2',
+                                        isDirectory: false
+                                    }
+                                ]
+                            },
+                            {
+                                filename: 'a.2',
+                                isDirectory: true,
+                                children: [
+                                    {
+                                        filename: 'a.2.1',
+                                        isDirectory: true
+                                    },
+                                    {
+                                        filename: 'a.2.2',
+                                        isDirectory: false
+                                    },
+                                    {
+                                        filename: 'a.2.3',
+                                        isDirectory: false
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        filename: 'b',
+                        isDirectory: true,
+                        children: [
+                            {
+                                filename: 'b.1',
+                                isDirectory: true,
+                                children: [
+                                    {
+                                        filename: 'b.1.1',
+                                        isDirectory: true
+                                    },
+                                    {
+                                        filename: 'b.1.2',
+                                        isDirectory: false
+                                    }
+                                ]
+                            },
+                            {
+                                filename: 'b.2',
+                                isDirectory: true,
+                                children: [
+                                    {
+                                        filename: 'b.2.1',
+                                        isDirectory: false
+                                    },
+                                    {
+                                        filename: 'b.2.2',
+                                        isDirectory: true
+                                    },
+                                    {
+                                        filename: 'b.2.3',
+                                        isDirectory: false
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        filename: 'c',
+                        isDirectory: true,
+                        children: [
+                            {
+                                filename: 'c.1',
+                                isDirectory: true,
+                                children: [
+                                    {
+                                        filename: 'c.1.1',
+                                        isDirectory: false
+                                    },
+                                    {
+                                        filename: 'c.1.2',
+                                        isDirectory: true
+                                    }
+                                ]
+                            },
+                            {
+                                filename: 'c.2',
+                                isDirectory: true,
+                                children: [
+                                    {
+                                        filename: 'c.2.1',
+                                        isDirectory: true
+                                    },
+                                    {
+                                        filename: 'c.2.2',
+                                        isDirectory: false
+                                    },
+                                    {
+                                        filename: 'c.2.3',
+                                        isDirectory: false
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            };
+        }
+    }
+  
+});
