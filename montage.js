4642c0175dcdaae97a97c9f4e2ef1fecebc10516
Corentin Debost <codebost@gmail.com>
2017-12-15 17:03:00 -0800

    Add a TemplateLoader to montage bootstrap for nodejs
    Used to parse dependencies of html files and serialization, needed to get a proper mop build that does load extra stuff asynchronously.

--- montage.js
+++ montage.js
@@ -382,10 +382,142 @@
     exports.initNodeJS = function initServer() {
 
         var PATH = require("path"),
-            FS  = require("fs");
+            FS  = require("fs"),
+            URL = require("url"),
+            htmlparser = require("htmlparser2"),
+            DomUtils = htmlparser.DomUtils;
 
-        return  {
+        function parseHtml(html) {
+            var dom, error;
 
+            var handler = new htmlparser.DomHandler(function (_error, _dom) {
+                error = _error;
+                dom = _dom;
+            });
+
+            // although these functions use callbacks they are actually synchronous
+            var parser = new htmlparser.Parser(handler);
+            parser.write(html);
+            parser.done();
+
+            if (error) {
+                throw error;
+            } else if (!dom) {
+                throw new Error("HTML parsing did not complete");
+            }
+
+            // wrap the returned array in a pseudo-document object for consistency
+            return {type: "document", children: dom};
+        }
+
+        function visit(element, visitor) {
+            var pruned;
+            var prune = function () {
+                pruned = true;
+            };
+            visitor(element, prune);
+            if (pruned) {
+                return;
+            }
+
+            var children = element.children;
+            var len = children ? children.length : 0;
+            for (var i = 0; i < len; i++) {
+                visit(children[i], visitor);
+            }
+        }
+
+        function getAttribute(element, name) {
+            return element.attribs ? element.attribs[name] : null;
+        }
+
+        function getText(element) {
+            return DomUtils.getText(element);
+        }
+
+        function parsePrototypeForModule(prototype) {
+            return prototype.replace(/\[[^\]]+\]$/, "");
+        }
+
+        function collectSerializationDependencies(text, dependencies) {
+            var serialization = JSON.parse(text);
+            Object.keys(serialization).forEach(function (label) {
+                var description = serialization[label];
+                if (description.lazy) {
+                    return;
+                }
+                if (typeof description.prototype === "string") {
+                    dependencies.push(parsePrototypeForModule(description.prototype));
+                }
+                if (typeof description.object === "string") {
+                    dependencies.push(parsePrototypeForModule(description.object));
+                }
+            });
+            return dependencies;
+        }
+
+        function collectHtmlDependencies(dom, dependencies) {
+            visit(dom, function (element) {
+                if (DomUtils.isTag(element)) {
+                    if (element.name === "script") {
+                        if (getAttribute(element, "type") === "text/montage-serialization") {
+                            collectSerializationDependencies(getText(element), dependencies);
+                        }
+                    } else if (element.name === "link") {
+                        if (getAttribute(element, "type") === "text/montage-serialization") {
+                            dependencies.push(getAttribute(element, "href"));
+                        }
+                    }
+                }
+            });
+        }
+
+        function parseHtmlDependencies(text/*, location*/) {
+            var dependencies = [];
+            var dom = parseHtml(text);
+            collectHtmlDependencies(dom, dependencies);
+            return dependencies;
+        }
+
+        exports.TemplateLoader = function (config, load) {
+            return function (id, module) {
+                var html = id.match(/(.*\/)?(?=[^\/]+\.html$)/);
+                var serialization = id.match(/(?=[^\/]+\.(?:json|mjson|meta)$)/); // XXX this is not necessarily a strong indicator of a serialization alone
+                var reelModule = id.match(/(.*\/)?([^\/]+)\.reel\/\2$/);
+                if (html) {
+                    return load(id, module)
+                    .then(function () {
+                        module.dependencies = parseHtmlDependencies(module.text, module.location);
+                        return module;
+                    });
+                } else if (serialization) {
+                    return load(id, module)
+                    .then(function () {
+                        module.dependencies = collectSerializationDependencies(module.text, []);
+                        return module;
+                    });
+                } else if (reelModule) {
+                    return load(id, module)
+                    .then(function () {
+                        var reelHtml = URL.resolve(module.location, reelModule[2] + ".html");
+                        return FS.stat(URL.parse(reelHtml).pathname)
+                        .then(function (stat) {
+                            if (stat.isFile()) {
+                                module.extraDependencies = [id + ".html"];
+                            }
+                        }, function (error) {
+                            // not a problem
+                            // montage/ui/loader.reel/loader.html": Error: ENOENT: no such file or directory
+                            console.log(error.message);
+                        });
+                    });
+                } else {
+                    return load(id, module);
+                }
+            };
+        };
+
+        return {
             _location: null,
 
             getLocation: function () {
@@ -443,9 +575,15 @@
             },
 
             bootstrap: function (callback) {
-
                 var self = this,
                     params = self.getParams();
+
+                // add the TemplateLoader to the middleware chain
+                mr.makeLoader = (function (makeLoader) {
+                    return function (config) {
+                        return exports.TemplateLoader(config, makeLoader(config));
+                    };
+                })(mr.makeLoader);
 
                 if (params.package) {
                     callback(mr, Promise, miniURL);
