289387b4694b7e979b532b242104af8ed8c4cfe3
Thibault Zanini <thibaultzanini@gmail.com>
2018-02-09 15:10:53 -0500

    Cleaning up List.info sample

--- ui/list.info/sample/ui/main.reel/main.js
+++ ui/list.info/sample/ui/main.reel/main.js
@@ -1,75 +1,13 @@
 var Component = require("montage/ui/component").Component,
-    Employee = require("../../core/model/employee").Employee;    
+    MockService = require('montage/test/mocks/data/services/mock-service').MockService;
 
 exports.Main = Component.specialize(/** @lends Main# */{
 
     constructor: {
         value: function () {
-            var data = [
-                    {
-                        firstname: 'Annette',
-                        lastname: 'Alarcon',
-                        department: 'sales',
-                        id: 1001
-                    },
-                    {
-                        firstname: 'Fredricka',
-                        lastname: 'Fuss',
-                        department: 'accounting',
-                        id: 1101
-                    },
-                    {
-                        firstname: 'Bev',
-                        lastname: 'Burney',
-                        department: 'accounting',
-                        id: 1102
-                    },
-                    {
-                        firstname: 'Moriah',
-                        lastname: 'Mcwhorter',
-                        department: 'logistics',
-                        id: 1201
+            this.mockService = new MockService();
 
-                    },
-                    {
-                        firstname: 'Emanuel',
-                        lastname: 'Sullivan',
-                        department: 'management',
-                        id: 1301
-                    },
-                    {
-                        firstname: 'Gretchen',
-                        lastname: 'Chapman',
-                        department: 'accounting',
-                        id: 1103
-                    },
-                    {
-                        firstname: 'Courtney',
-                        lastname: 'Simon',
-                        department: 'logistics',
-                        id: 1202
-
-                    },
-                    {
-                        firstname: 'Brett',
-                        lastname: 'Lucas',
-                        department: 'management',
-                        id: 1302
-                    }
-                ],
-                employees = [],
-                employee;
-
-            for (var i = 0, length = data.length; i < length; i++) {
-                employee = new Employee();
-                employee.firstname = data[i].firstname;
-                employee.lastname = data[i].lastname;
-                employee.id = data[i].id;
-                employee.department = data[i].department;
-                employees.push(employee);
-            }
-
-            this.content = employees;
+            this.content = this.mockService.fetchEmployees();
         }
     }
   
