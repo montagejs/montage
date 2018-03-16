b3b169467cc812cdde5182a0acad6aa36d7ba7ee
Jonathan Miller <jonathan.miller@kaazing.com>
2018-03-16 10:30:40 -1000

    Fixes another merge error

--- data/service/raw-data-service.js
+++ data/service/raw-data-service.js
@@ -807,7 +807,7 @@ exports.RawDataService = DataService.specialize(/** @lends RawDataService.protot
      */
      mapRawDataToObject: {
         value: function (rawData, object, context) {
-            return this.mapFromRawData(object, record, context);
+            return this.mapFromRawData(object, rawData, context);
         }
     },
 
