58b55543a889298bbb9db1c8732eb7dc5e58c523
Thibault Zanini <thibaultzanini@gmail.com>
2018-03-20 16:42:32 -0400

    Refactor Toggle Component.

--- ui/toggle.reel/toggle.js
+++ ui/toggle.reel/toggle.js
@@ -1,76 +1,7 @@
-var Control = require("../control").Control,
-    PressComposer = require("../../composer/press-composer").PressComposer;
+var CheckControl = require("../check-control").CheckControl;
 
 /**
  * @class Toggle
- * @extends Control
+ * @extends CheckControl
  */
-var Toggle = exports.Toggle = Control.specialize({
-
-    checked: {
-        value: false
-    },
-
-    __pressComposer: {
-        value: null
-    },
-
-    _pressComposer: {
-        get: function () {
-            if (!this.__pressComposer) {
-                this.__pressComposer = new PressComposer();
-                this.addComposer(this.__pressComposer);
-            }
-
-            return this.__pressComposer;
-        }
-    },
-
-    enterDocument: {
-        value: function () {
-            this._startListeningToPressIfNeeded();
-        }
-    },
-
-    prepareForActivationEvents: {
-        value: function () {
-            this._startListeningToPress();
-        }
-    },
-
-    exitDocument: {
-        value: function () {
-            this._startListeningToPressIfNeeded();
-        }
-    },
-
-    _startListeningToPressIfNeeded: {
-        value: function () {
-            if (this.preparedForActivationEvents) {
-                this._startListeningToPress();
-            }
-        }
-    },
-
-    _startListeningToPress: {
-        value: function () {
-            this._pressComposer.addEventListener('press', this);
-        }
-    },
-
-    _stopListeningToPress: {
-        value: function () {
-            if (this.preparedForActivationEvents) {
-                this._pressComposer.removeEventListener('press', this);
-            }
-        }
-    },
-
-    handlePress: {
-        value: function () {
-            this.checked = !this.checked;
-            this.dispatchActionEvent(this.checked);
-        }
-    }
-
-});
+var Toggle = exports.Toggle = CheckControl.specialize();
