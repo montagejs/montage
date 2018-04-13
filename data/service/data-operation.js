f5a3d9eeecf3e4fc9ca323b3c57d53d4ed97ffa0
Benoit Marchant <marchant@mac.com>
2018-04-12 17:49:46 -0700

    First batch of proposed changes to DataOperation, WIP

--- data/service/data-operation.js
+++ data/service/data-operation.js
@@ -1,4 +1,5 @@
-var Montage = require("core/core").Montage;
+var Montage = require("core/core").Montage,
+    Criteria = require("montage/core/criteria").Criteria;
 
 /**
  * Represents
@@ -36,6 +37,16 @@ exports.DataOperation = Montage.specialize(/** @lends DataOperation.prototype */
     },
 
     /**
+     * A property used to give a meaningful name to an operation
+     *
+     * @type {string}
+     */
+    name: {
+        value: undefined
+    },
+    /**
+     *  BENOIT: we have an overlap in term of semantics between the type of the operation and the type of the data it applies to. So we either keep "type" for the operation itseld as it is, and dataType, or we flip, calling this operationType and the dataType becomes "type".
+
      * @type {DataOperation.Type.CREATE|DataOperation.Type.READ|DataOperation.Type.UPDATE|DataOperation.Type.DELETE}
      */
     type: {
@@ -43,6 +54,34 @@ exports.DataOperation = Montage.specialize(/** @lends DataOperation.prototype */
     },
 
     /**
+     * The criteria that qualifies objects this operation applies to.
+     * For a create operation it may not apply.
+     * For a read operation it would be the criteria of the DataQuery.
+     * For an update, it describes the set of objects to receive the changes
+     * carried in this operation. It could be an "or" of primary keys to a more
+     * model-related set of property/values.
+     * For a delete, it would describes objects to delete.
+     * Expected to be a boolean expression to be applied to data
+     * objects to determine whether they should be impacted by this operation or not.
+     *
+     * @type {Criteria}
+     */
+    criteria: {
+        get: function () {
+            return this._criteria;
+        },
+        set: function (criteria) {
+            this._criteria = criteria;
+        }
+    },
+
+    _criteria: {
+        value: undefined
+    },
+
+
+    /**
+     * BENOIT: should it be called timestamp instead?
      * A number used to order operations according to when they were created.
      *
      * This is initialized when an operation is created to the value of
@@ -58,6 +97,33 @@ exports.DataOperation = Montage.specialize(/** @lends DataOperation.prototype */
      * @type {number}
      */
     time: {
+        value: undefined
+    },
+
+    /**
+     * An operation that preceded and this one is related to. For a ReadUpdated, it would be the Read operation.
+     *
+     * @type {DataOperation}
+     */
+    referer: {
+        value: undefined
+    },
+
+    /**
+     * A way to uniquely express the client/agent that created the operation.
+     *
+     * @type {Object}
+     */
+    origin: {
+        value: undefined
+    },
+
+    /**
+     * The authorization object representing an authenticated user, like a JWToken.
+     *
+     * @type {Object}
+     */
+    authorization: {
         value: undefined
     },
 
@@ -92,6 +158,7 @@ exports.DataOperation = Montage.specialize(/** @lends DataOperation.prototype */
     },
 
     /**
+     * Benoit: This property's role is a bit fuzzy. context can be changing and arbitrary. Keep??
      * @type {Object}
      */
     context: {
@@ -102,6 +169,11 @@ exports.DataOperation = Montage.specialize(/** @lends DataOperation.prototype */
      * Data Properties
      */
 
+    /**
+     * Benoit: I don't think this is needed as it's covered in a more generic
+     * way by the criteria property. Remove?
+     * @type {Object}
+     */
     dataID: {
         value: undefined
     },
@@ -149,12 +221,53 @@ exports.DataOperation = Montage.specialize(/** @lends DataOperation.prototype */
 
     Type: {
         value: {
-            CREATE: {isCreate: true},
-            READ: {isRead: true},
-            UPDATE: {isUpdate: true},
-            DELETE: {isDelete: true}
+            Create: {isCreate: true},
+            CreateFailed: {isCreate: true},
+            CreateCompleted: {isCreate: true},
+            /* Read is the first operation that mnodels a query */
+            Read: {isRead: true},
+
+            /* ReadUpdated is pushed by server when a query's result changes due to data changes from others */
+            ReadUpdated: {isRead: true},
+
+            /* ReadProgress / ReadUpdate / ReadSeek is used to instruct server that more data is required for a "live" read / query
+                Need a better name, and a symetric? Or is ReadUpdated enough if it referes to previous operation
+            */
+            ReadProgress: {isRead: true},
+
+            /* ReadCancel is the operation that instructs baclkend that client isn't interested by a read operastion anymore */
+            ReadCancel: {isRead: true},
+
+            /* ReadCanceled is the operation that instructs the client that a read operation is canceled */
+            ReadCanceled: {isRead: true},
+
+             /* ReadFailed is the operation that instructs the client that a read operation has failed canceled */
+            ReadFailed: {isRead: true},
+            /* ReadCompleted is the operation that instructs the client that a read operation has returned all available data */
+            ReadCompleted: {isRead: true},
+            Update: {isUpdate: true},
+            UpdateCompleted: {isUpdate: true},
+            Delete: {isDelete: true},
+            DeleteCompleted: {isDelete: true},
+            DeleteFailed: {isDelete: true},
+            /* Lock models the ability for a client to prevent others to make changes to a set of objects described by operation's criteria */
+            Lock: {isLock true},
+            LockCompleted: {isLock true},
+            LockFailed: {isLock true},
+            /* RemmoteProcedureCall models the ability to invoke code logic on the server-side, being a DB StoredProcedure, or an method/function in a service */
+            RemmoteProcedureCall: {isLock true},
+            RemmoteProcedureCallCompleted: {isLock true},
+            RemmoteProcedureCallFailed: {isLock true}
         }
     }
+
+    /*
+        For update, needs to model:
+            - property value changed, needed for properties with cardinality 1 or n
+            - property added / removed for properties with cardinality n
+
+            - snapshot of known values that changed?
+    */
 
 });
 
