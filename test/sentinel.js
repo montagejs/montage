67a25150774987ac198d9eac537adad3b3a00b73
Thomas Jaede <thomas.jaede@kaazing.com>
2018-05-12 14:26:12 -0700

    Fix criteria reference

--- test/sentinel.js
+++ test/sentinel.js
@@ -3,6 +3,7 @@ console.log('montage-testing', 'Start');
 module.exports = require("montage-testing").run(require, [
 
     {name: "spec/data/raw-data-worker"}
+    
 ]).then(function () {
     console.log('montage-testing', 'End');
 }, function (err) {
