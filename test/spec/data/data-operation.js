b0f1f1d46d88d6ab43fe51ffeb782681584de8ef
Thomas Jaede <thomas.jaede@kaazing.com>
2018-05-12 14:31:53 -0700

    Fix data-operation require of criteria & add spec

---
+++ test/spec/data/data-operation.js
@@ -0,0 +1,9 @@
+var DataOperation = require("montage/data/service/data-operation").DataOperation;
+
+describe("A DataOperation", function() {
+
+    it("can be created", function () {
+        expect(new DataOperation()).toBeDefined();
+    });
+
+});
