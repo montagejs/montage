5ee50b79122bba99592c4a6140805337a495c321
Thomas Jaede <thomas.jaede@kaazing.com>
2018-06-20 14:44:39 -0700

    Add specs for auth failure and re-auth

--- test/spec/data/logic/authorization/authorization-service.js
+++ test/spec/data/logic/authorization/authorization-service.js
@@ -17,10 +17,50 @@ exports.AuthorizationService = DataService.specialize( /** @lends AuthorizationS
     
     authorize: {
         value: function () {
-            return Promise.resolve(this.authorization);
+            return this.promiseDescriptor.value;
+        }
+    },
+
+    reset: {
+        value: function () {
+            this._promiseDescriptor = this._makePromiseDescriptor();
+        }
+    },
+
+    _makePromiseDescriptor: {
+        value: function () {
+            var descriptor = {};
+            descriptor.value = new Promise(function (resolve, reject) {
+                descriptor.resolve = resolve;
+                descriptor.reject = reject;
+            });
+            return descriptor;
+        }
+    },
+
+
+    promiseDescriptor: {
+        get: function () {
+            var self = this,
+                descriptor;
+           if (!this._promiseDescriptor) {
+                this._promiseDescriptor = this._makePromiseDescriptor();
+           } 
+           return this._promiseDescriptor;
+        }
+    },
+
+    resolve: {
+        value: function () {
+            this.promiseDescriptor.resolve(this.authorization);
+        }
+    },
+
+    reject: {
+        value: function () {
+            this.promiseDescriptor.reject(new Error("Test AuthService Rejection"));
         }
     }
-
     
 
 });
