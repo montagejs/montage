5f6c6a7afb86fc993d227f09aaa72757655a566c
Benoit Marchant <marchant@mac.com>
2019-11-25 22:44:51 -0800

    Fix a bug in handleOrganizedContentRangeChange that impacts every listeners dispatched after handleOrganizedContentRangeChange because it modifies the passed argument.

--- core/range-controller.js
+++ core/range-controller.js
@@ -778,6 +778,16 @@ var RangeController = exports.RangeController = Montage.specialize( /** @lends R
 
                 if (this.selection.length) {
                     this.selection.deleteEach(diff);
+<<<<<<< HEAD
+=======
+
+                    // ensure selection always has content
+                    if (this.selection.length === 0 && this.content && this.content.length &&
+                        this.avoidsEmptySelection && !this.allowsMultipleSelection) {
+                        // selection can't contain previous content value as content already changed
+                        this.selection.add(this.content[this.content.length - 1]);
+                    }
+>>>>>>> Fix a bug in handleOrganizedContentRangeChange
                 }
             }
 
