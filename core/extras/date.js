4be0a2998631b55f7699d9c13d03254093da49a9
Benoit Marchant <marchant@mac.com>
2020-07-16 00:52:15 -0700

    remove "export" from comments

--- core/extras/date.js
+++ core/extras/date.js
@@ -308,36 +308,36 @@ Object.defineProperty(Date.prototype, "isToday", {
 */
 
 /*
-export- const SECONDS_A_MINUTE = 60
-export- const SECONDS_A_HOUR = SECONDS_A_MINUTE * 60
-export- const SECONDS_A_DAY = SECONDS_A_HOUR * 24
-export- const SECONDS_A_WEEK = SECONDS_A_DAY * 7
+exports- const SECONDS_A_MINUTE = 60
+exports- const SECONDS_A_HOUR = SECONDS_A_MINUTE * 60
+exports- const SECONDS_A_DAY = SECONDS_A_HOUR * 24
+exports- const SECONDS_A_WEEK = SECONDS_A_DAY * 7
 
-export- const MILLISECONDS_A_SECOND = 1e3
-export- const MILLISECONDS_A_MINUTE = SECONDS_A_MINUTE * MILLISECONDS_A_SECOND
-export- const MILLISECONDS_A_HOUR = SECONDS_A_HOUR * MILLISECONDS_A_SECOND
-export- const MILLISECONDS_A_DAY = SECONDS_A_DAY * MILLISECONDS_A_SECOND
-export- const MILLISECONDS_A_WEEK = SECONDS_A_WEEK * MILLISECONDS_A_SECOND
+exports- const MILLISECONDS_A_SECOND = 1e3
+exports- const MILLISECONDS_A_MINUTE = SECONDS_A_MINUTE * MILLISECONDS_A_SECOND
+exports- const MILLISECONDS_A_HOUR = SECONDS_A_HOUR * MILLISECONDS_A_SECOND
+exports- const MILLISECONDS_A_DAY = SECONDS_A_DAY * MILLISECONDS_A_SECOND
+exports- const MILLISECONDS_A_WEEK = SECONDS_A_WEEK * MILLISECONDS_A_SECOND
 
 // English locales
-export- const MS = 'millisecond'
-export- const S = 'second'
-export- const MIN = 'minute'
-export- const H = 'hour'
-export- const D = 'day'
-export- const W = 'week'
-export- const M = 'month'
-export- const Q = 'quarter'
-export- const Y = 'year'
-export- const DATE = 'date'
+exports- const MS = 'millisecond'
+exports- const S = 'second'
+exports- const MIN = 'minute'
+exports- const H = 'hour'
+exports- const D = 'day'
+exports- const W = 'week'
+exports- const M = 'month'
+exports- const Q = 'quarter'
+exports- const Y = 'year'
+exports- const DATE = 'date'
 
-export- const FORMAT_DEFAULT = 'YYYY-MM-DDTHH:mm:ssZ'
+exports- const FORMAT_DEFAULT = 'YYYY-MM-DDTHH:mm:ssZ'
 
-export- const INVALID_DATE_STRING = 'Invalid Date'
+exports- const INVALID_DATE_STRING = 'Invalid Date'
 
 // regex
-export- const REGEX_PARSE = /^(\d{4})-?(\d{1,2})-?(\d{0,2})[^0-9]*(\d{1,2})?:?(\d{1,2})?:?(\d{1,2})?.?(\d{1,3})?$/
-export- const REGEX_FORMAT = /\[([^\]]+)]|Y{2,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a|A|m{1,2}|s{1,2}|Z{1,2}|SSS/g
+exports- const REGEX_PARSE = /^(\d{4})-?(\d{1,2})-?(\d{0,2})[^0-9]*(\d{1,2})?:?(\d{1,2})?:?(\d{1,2})?.?(\d{1,3})?$/
+exports- const REGEX_FORMAT = /\[([^\]]+)]|Y{2,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a|A|m{1,2}|s{1,2}|Z{1,2}|SSS/g
 */
 
 /*
