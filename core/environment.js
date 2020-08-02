676f9a74d16a75ba9d0161e103042ec09d826987
Benoit Marchant <marchant@mac.com>
2020-08-01 23:41:51 -0700

    fix exception in node

--- core/environment.js
+++ core/environment.js
@@ -18,7 +18,9 @@ var Environment = exports.Environment = Montage.specialize({
 
     systemLocaleIdentifier: {
         get: function () {
-            return (navigator.languages && navigator.languages.length) ? navigator.languages[0] : navigator.userLanguage || navigator.language || navigator.browserLanguage || 'en'
+            return typeof navigator === "object"
+                ? (navigator.languages && navigator.languages.length) ? navigator.languages[0] : navigator.userLanguage || navigator.language || navigator.browserLanguage || 'en'
+                : "en"
         }
     },
 
