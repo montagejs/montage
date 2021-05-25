47df21650340aaab5d15adf2f145a3b181edae06
Benoit Marchant <marchant@mac.com>
2021-05-25 16:53:30 -0700

    tweak deserialization to only actually add content once the whole serialization is complete, which is more exact.

--- core/extras/map.js
+++ core/extras/map.js
@@ -11,28 +11,54 @@
     */
     Object.defineProperty(Map.prototype, "deserializeSelf", {
         value: function (deserializer) {
-            var entries, keys, values,
-                i, n;
+            var entries, keys, values;
 
             entries = deserializer.getProperty("entries");
             if (entries) {
-                for (i = 0, n = entries.length; i < n; i++) {
-                    this.set(entries[i].key, entries[i].value);
-                }
-            } 
-            if (!entries) {
+                this._entries = entries;
+            } else {
                 keys = deserializer.getProperty("keys");
                 values = deserializer.getProperty("values");
                 if (keys && values) {
-                    for (i = 0, n = keys.length; i < n; i++) {
-                        this.set(keys[i], values[i]);
-                    }
+                    this._keys = keys;
+                    this._values = values;
                 }
             }
         },
         writable: true,
         configurable: true
     });
+    Object.defineProperty(Map.prototype, "deserializedFromSerialization", {
+
+        value: function (label) {
+
+            if(this._entries) {
+                var entries = this._entries, i, n;
+                for (i = 0, n = entries.length; i < n; i++) {
+                    this.set(entries[i].key, entries[i].value);
+                }
+
+                if(this._entries.length !== this.size) {
+                    console.warn("deserialized entries likely overlapped keys resulting in lost content");
+                }
+                delete this._entries;
+
+            } else if(this._keys && this._values) {
+                var keys = this._keys,
+                    values = this._values,
+                    i, n;
+                for (i = 0, n = keys.length; i < n; i++) {
+                    this.set(keys[i], values[i]);
+                }
+
+                delete this._keys;
+                delete this._values;
+            }
+        },
+        writable: true,
+        configurable: true
+    });
+
 
 
     /**
@@ -51,11 +77,12 @@
                 serializedKeys.push(entry[0]);
                 serializedValues.push(entry[1]);
             }
-            
+
             serializer.setProperty("keys", serializedKeys);
             serializer.setProperty("values", serializedValues);
-            
+
         },
         writable: true,
         configurable: true
     });
+
