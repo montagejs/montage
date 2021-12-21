bf9d05a4eade01e41d6dc63ca1eaf7ccca11ac46
Benoit Marchant <marchant@mac.com>
2021-12-21 13:29:40 -0800

    remove .meta support

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
             included: false
         },
         {
-            pattern: 'ui/**/*.meta',
+            pattern: 'ui/**/*.mjson',
             included: false
         },
         {
@@ -77,10 +77,6 @@ module.exports = function(config) {
         },
         {
             pattern: 'test/**/*.html',
-            included: false
-        },
-        {
-            pattern: 'test/**/*.meta',
             included: false
         },
         {
