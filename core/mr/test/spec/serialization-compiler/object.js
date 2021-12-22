98280e1d5212d83e15cc56f7a581bb22fe705b43
Benoit Marchant <marchant@mac.com>
2021-12-21 16:06:05 -0800

    fix test regressions involving expectations of Montage.getInfoForObject presence to get ._montage_metadata created

--- core/mr/test/spec/serialization-compiler/object.js
+++ core/mr/test/spec/serialization-compiler/object.js
@@ -1,3 +1,5 @@
 var Model = require('./model').Model;
+var Montage = require("montage").Montage;
 
-exports.model = new Model(10)
+Model.getInfoForObject = Montage.getInfoForObject;
+exports.model = new Model(10);
