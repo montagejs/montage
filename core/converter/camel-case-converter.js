c30534852a27026a9014f6d3738c3edb46a64540
Benoit Marchant <marchant@mac.com>
2022-01-17 16:56:56 -0800

    - inline just-camel-case module as needed to add the ability to uppercase the first letter as an option. Faster to do it as we go remove just-camel-case dependency from montage

--- core/converter/camel-case-converter.js
+++ core/converter/camel-case-converter.js
@@ -1,9 +1,16 @@
 /**
  * @module montage/core/converter/camel-case-converter
  * @requires montage/core/converter/converter
+ * borrowed from https://github.com/angus-c/just/blob/master/packages/string-camel-case/index.js (MIT)
  */
 var Converter = require("./converter").Converter,
-    camelCase = require('just-camel-case'),
+
+    // any combination of spaces and punctuation characters
+    // thanks to http://stackoverflow.com/a/25575009
+    wordSeparatorsRegEx = /[\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]+/,
+    basicCamelRegEx = /^[a-z\u00E0-\u00FCA-Z\u00C0-\u00DC][\d|a-z\u00E0-\u00FCA-Z\u00C0-\u00DC]*$/,
+    fourOrMoreConsecutiveCapsRegEx = /([A-Z\u00C0-\u00DC]{4,})/g,
+    allCapsRegEx = /^[A-Z\u00C0-\u00DC]+$/,
     singleton;
 
 /**
@@ -28,8 +35,46 @@ var CamelCaseConverter = exports.CamelCaseConverter = Converter.specialize({
         }
     },
 
+    _deCap: {
+        value: function deCap(match, endOfWord) {
+        var arr = match.split('');
+        var first = arr.shift().toUpperCase();
+        var last = endOfWord ? arr.pop().toLowerCase() : arr.pop();
+        return first + arr.join('').toLowerCase() + last;
+      }
+    },
+
+    convertFirstLetterToUpperCase: {
+        value: false
+    },
+
     convert: {
-        value: camelCase
+        value: function CamelCaseConverter_convert(str) {
+
+            var words = str.split(wordSeparatorsRegEx),
+                len = words.length,
+                mappedWords = new Array(len),
+                decap = this._deCap,
+                convertFirstLetterToUpperCase = this.convertFirstLetterToUpperCase,
+                word, isCamelCase, firstLetter;
+
+            for (var i = 0; i < len; i++) {
+              word = words[i];
+              if (word === '') {
+                continue;
+              }
+              isCamelCase = basicCamelRegEx.test(word) && !allCapsRegEx.test(word);
+              if (isCamelCase) {
+                word = word.replace(fourOrMoreConsecutiveCapsRegEx, function(match, p1, offset) {
+                  return deCap(match, word.length - offset - match.length == 0);
+                });
+              }
+              firstLetter = word[0];
+              firstLetter = (convertFirstLetterToUpperCase || i > 0) ? firstLetter.toUpperCase() : firstLetter.toLowerCase();
+              mappedWords[i] = firstLetter + (!isCamelCase ? word.slice(1).toLowerCase() : word.slice(1));
+            }
+            return mappedWords.join('');
+          }
     }
 
 });
